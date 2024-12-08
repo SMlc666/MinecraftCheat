849201eddbcb19a69a4b7250e48ba112f1e5bac3
SMlc666 <qq1030452538@outlook.com>
2024-12-08 15:03:46 +0800

    格式化代码

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,16 @@
 #include "imgui.h"
 #include "backends/imgui_impl_android.h"
 #include "backends/imgui_impl_opengl3.h"
-JNIEnv* env = nullptr;
-JavaVM* jvm = nullptr;
-void setup(){
-    dlopen("libEGL.so",4);
+JNIEnv *env = nullptr;
+JavaVM *jvm = nullptr;
+void setup() {
+  dlopen("libEGL.so", 4);
 }
-extern "C" jint JNIEXPORT JNI_OnLoad(JavaVM* vm, void *key)
-{
-    jvm = vm;
-    JNIEnv *m_env = nullptr;
-    vm->GetEnv((void**)&m_env, JNI_VERSION_1_6);
-    env = m_env;
-    std::thread(setup).detach();
-    return JNI_VERSION_1_6;
+extern "C" jint JNIEXPORT JNI_OnLoad(JavaVM *vm, void *key) {
+  jvm = vm;
+  JNIEnv *m_env = nullptr;
+  vm->GetEnv((void **)&m_env, JNI_VERSION_1_6);
+  env = m_env;
+  std::thread(setup).detach();
+  return JNI_VERSION_1_6;
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,4 +1,4 @@
 #pragma once
 #include <jni.h>
-extern JNIEnv* env;
-extern JavaVM* jvm;
+extern JNIEnv *env;
+extern JavaVM *jvm;
