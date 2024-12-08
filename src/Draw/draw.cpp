d7d087d7738eea6a154b8f10a3be6df80bc62ca7
SMlc666 <qq1030452538@outlook.com>
2024-12-08 16:22:25 +0800

    imgui测试

--- src/Draw/draw.cpp
+++ src/Draw/draw.cpp
@@ -3,8 +3,22 @@
 #include "my_imgui.h"
 int g_GlHeight, g_GlWidth = 0;
 bool is_ImguiSetup = false;
+void imguiSetup() {
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  io.IniFilename = nullptr;
+  io.DisplaySize = ImVec2(1280, 720);
+  io.DisplaySize = ImVec2((float)g_GlWidth, (float)g_GlHeight);
+  ImGui_ImplAndroid_Init(nullptr);
+  ImGui_ImplOpenGL3_Init("#version 300 es");
+  ImGui::StyleColorsLight();
+  ImGui::Android_LoadSystemFont(22.0f);
+  ImGui::GetStyle().ScaleAllSizes(3.0f);
+}
 EGLBoolean (*old_eglSwapBuffers)(EGLDisplay dpy, EGLSurface surface);
 EGLBoolean my_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
+
   eglQuerySurface(dpy, surface, EGL_WIDTH, &g_GlWidth);
   eglQuerySurface(dpy, surface, EGL_HEIGHT, &g_GlHeight);
   if (!is_ImguiSetup) {
@@ -19,24 +33,28 @@ EGLBoolean my_eglSwapBuffers(EGLDisplay dpy, EGLSurface surface) {
   ImGui::EndFrame();
   ImGui::Render();
   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-  glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+  glViewport(0, 0, g_GlWidth, g_GlHeight);
   return old_eglSwapBuffers(dpy, surface);
 }
-void imguiSetup() {
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO &io = ImGui::GetIO();
-  io.IniFilename = nullptr;
-  io.DisplaySize = ImVec2(1280, 720);
-  io.DisplaySize = ImVec2((float)g_GlWidth, (float)g_GlHeight);
-  ImGui_ImplAndroid_Init(nullptr);
-  ImGui_ImplOpenGL3_Init("#version 300 es");
-  ImGui::StyleColorsLight();
-  ImGui::Android_LoadSystemFont(22.0f);
-  ImGui::GetStyle().ScaleAllSizes(3.0f);
+void (*old_Input)(void *thiz, void *ex_ab, void *ex_ac);
+void my_Input(void *thiz, void *ex_ab, void *ex_ac) {
+  // 调用原始函数
+  old_Input(thiz, ex_ab, ex_ac);
+
+  // 处理输入事件
+  ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz);
+
+  return;
 }
+
 void drawSetup() {
   void *egl_handle = dlopen("libEGL.so", 4);
   void *eglSwapBuffers = dlsym(egl_handle, "eglSwapBuffers");
   DobbyHook((void *)eglSwapBuffers, (void *)my_eglSwapBuffers, (void **)&old_eglSwapBuffers);
+  void *sym_input = DobbySymbolResolver(
+      nullptr,
+      "_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE");
+  if (sym_input != nullptr) {
+    DobbyHook((void *)sym_input, (void *)my_Input, (void **)&old_Input);
+  }
 }
