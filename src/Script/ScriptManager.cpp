b5e0a4371c8492a7a53896a79fb09ec28bbf956b
SMlc666 <qq1030452538@outlook.com>
2024-12-15 14:00:29 +0800

    完善

--- src/Script/ScriptManager.cpp
+++ src/Script/ScriptManager.cpp
@@ -1,19 +1,24 @@
 #include <filesystem>
 #include "ScriptManager.hpp"
+#include "log.hpp"
 #include <mutex>
+#include <format>
 #include <vector>
 namespace ScriptManager {
 std::vector<ScriptInfo> Scripts = {};
 std::mutex ScriptsMutex;
-void addScript(const std::string &name) {
+void addScript(const std::filesystem::path &path) {
+  std::lock_guard<std::mutex> lock(ScriptsMutex);
+  ScriptInfo info(path.string());
+  g_log_tool.message(LogLevel::INFO, "ScriptManager", std::format("Add script: {}", info.file));
+  Scripts.emplace_back(info);
 }
 void reloadScripts(const std::filesystem::path &path) {
   std::lock_guard<std::mutex> lock(ScriptsMutex);
   Scripts.clear();
   for (const auto &entry : std::filesystem::directory_iterator(path)) {
     if (std::filesystem::is_regular_file(entry.path())) {
-      ScriptInfo info(entry.path().string());
-      Scripts.emplace_back(info);
+      addScript(entry.path());
     }
   }
 }
