34f06c2601c4da3d63454081d760dcf93061f02b
SMlc666 <qq1030452538@outlook.com>
2024-12-21 14:30:34 +0800

    优化

--- src/Module/menu/menu.cpp
+++ src/Module/menu/menu.cpp
@@ -3,31 +3,32 @@
 #include "log.hpp"
 #include "ScriptManager.hpp"
 #include "config/config.hpp"
+#include <thread>
 static void drawLog(std::vector<LogEntry> &Logs) {
   for (auto &log : Logs) {
     switch (log.level) {
     case LogLevel::DEBUG:
-      ImGui::TextColored(ImVec4(0.0f, 0.0f, 1.0f, 1.0f), "[%s] %s %s %s",
+      ImGui::TextColored(ImVec4(0.0F, 0.0F, 1.0F, 1.0F), "[%s] %s %s %s",
                          LogLevelNames.at(log.level).c_str(), log.getFormattedTime().c_str(),
                          log.tag.c_str(), log.message.c_str());
       break;
     case LogLevel::INFO:
-      ImGui::TextColored(ImVec4(1.0f, 1.0f, 1.0f, 1.0f), "[%s] %s %s %s",
+      ImGui::TextColored(ImVec4(1.0F, 1.0F, 1.0F, 1.0F), "[%s] %s %s %s",
                          LogLevelNames.at(log.level).c_str(), log.getFormattedTime().c_str(),
                          log.tag.c_str(), log.message.c_str());
       break;
     case LogLevel::WARN:
-      ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "[%s] %s %s %s",
+      ImGui::TextColored(ImVec4(1.0F, 1.0F, 0.0F, 1.0F), "[%s] %s %s %s",
                          LogLevelNames.at(log.level).c_str(), log.getFormattedTime().c_str(),
                          log.tag.c_str(), log.message.c_str());
       break;
     case LogLevel::ERROR:
-      ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "[%s] %s %s %s",
+      ImGui::TextColored(ImVec4(1.0F, 0.0F, 0.0F, 1.0F), "[%s] %s %s %s",
                          LogLevelNames.at(log.level).c_str(), log.getFormattedTime().c_str(),
                          log.tag.c_str(), log.message.c_str());
       break;
     case LogLevel::FATAL:
-      ImGui::TextColored(ImVec4(0.0f, 0.0f, 0.0f, 1.0f), "[%s] %s %s %s",
+      ImGui::TextColored(ImVec4(0.0F, 0.0F, 0.0F, 1.0F), "[%s] %s %s %s",
                          LogLevelNames.at(log.level).c_str(), log.getFormattedTime().c_str(),
                          log.tag.c_str(), log.message.c_str());
       break;
@@ -41,16 +42,19 @@ const std::unordered_map<MenuType, std::function<void()>> MenuFunctions = {
     {MenuType::COMBAT_MENU, []() { ImGui::Text("Combat Menu"); }},
     {MenuType::LOG_MENU,
      []() {
-       if (ImGui::Button("Clear"))
+       if (ImGui::Button("Clear")) {
          g_log_tool.cleanLogs();
+       }
        ImGui::SameLine();
-       if (ImGui::Button("Save"))
-         g_log_tool.SaveToFile();
+       if (ImGui::Button("Save")) {
+         std::thread([]() { g_log_tool.SaveToFile(); }).detach(); //线程分离
+       }
        if (ImGui::TreeNode("tags")) {
-         for (auto &tag : g_log_tool.getTagMap()) {
+         for (const auto &tag : g_log_tool.getTagMap()) {
            if (ImGui::TreeNode(tag.first.c_str())) {
-             if (ImGui::Button("Clear"))
+             if (ImGui::Button("Clear")) {
                g_log_tool.cleanLogs(tag.first);
+             }
              auto tagLogs = g_log_tool.getLogs(tag.first);
              drawLog(tagLogs);
              ImGui::TreePop();
@@ -67,8 +71,9 @@ const std::unordered_map<MenuType, std::function<void()>> MenuFunctions = {
     {MenuType::SCRIPT_MENU,
      []() {
        auto Scripts = ScriptManager::getScripts();
-       if (ImGui::Button("Reload Scripts"))
+       if (ImGui::Button("Reload Scripts")) {
          ScriptManager::reloadScripts();
+       }
        for (auto &script : Scripts) {
          if (ImGui::TreeNodeEx(script->getName().c_str())) {
            ImGui::Text("file: %s", script->getFile().c_str());
