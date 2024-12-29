142d665325ebfb21546ab56935f75830adfa1b82
SMlc666 <qq1030452538@outlook.com>
2024-12-29 16:17:59 +0800

    MemTool的完善

---
+++ src/MemTool/MemTool.cpp
@@ -0,0 +1,54 @@
+#include "MemTool.hpp"
+#include "KittyMemory/KittyMemory.hpp"
+#include "KittyMemory/KittyScanner.hpp"
+#include <string>
+
+//NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast)
+template <typename T> bool MemTool::write(T address, const void *data, size_t size) {
+  return KittyMemory::memWrite(reinterpret_cast<void *>(address), data, size);
+}
+template <typename T> bool MemTool::read(T address, void *data, size_t size) {
+  return KittyMemory::memRead(reinterpret_cast<void *>(address), data, size);
+}
+template <typename T, typename U> T MemTool::read(U address) {
+  T value;
+  if (read(address, &value, sizeof(T))) {
+    return value;
+  }
+  return {};
+}
+template <typename T, typename U> bool MemTool::write(T address, U value) {
+  return write(address, &value, sizeof(U));
+}
+std::string MemTool::getProcessName() {
+  return KittyMemory::getProcessName();
+}
+template <typename T> T MemTool::getModuleBase(const std::string &moduleName) {
+  KittyMemory::ProcMap Module = KittyMemory::getElfBaseMap(moduleName);
+  if (Module.isUnknown()) {
+    return {};
+  }
+  return reinterpret_cast<T>(Module.startAddress);
+}
+template <typename T> T MemTool::getModuleEnd(const std::string &moduleName) {
+  KittyMemory::ProcMap Module = KittyMemory::getElfBaseMap(moduleName);
+  if (Module.isUnknown()) {
+    return {};
+  }
+  return reinterpret_cast<T>(Module.endAddress);
+}
+KittyMemory::ProcMap MemTool::getModuleMap(const std::string &moduleName) {
+  KittyMemory::ProcMap Module = KittyMemory::getElfBaseMap(moduleName);
+  return Module;
+}
+template <typename T>
+T MemTool::findIdaPatternFirst(const std::string &moduleName, const std::string &pattern) {
+  KittyMemory::ProcMap Module = getModuleMap(moduleName);
+  if (Module.isUnknown()) {
+    return {};
+  }
+  return reinterpret_cast<T>(
+      KittyScanner::findIdaPatternFirst(Module.startAddress, Module.endAddress, pattern));
+}
+
+//NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast)
--- src/MemTool/MemTool.hpp
+++ src/MemTool/MemTool.hpp
@@ -1,1 +1,16 @@
 #pragma once
+#include "KittyMemory/KittyMemory.hpp"
+#include <cstddef>
+#include <string>
+namespace MemTool {
+template <typename T> bool write(T address, const void *data, size_t size);
+template <typename T> bool read(T address, void *data, size_t size);
+template <typename T, typename U> T read(U address);
+template <typename T, typename U> bool write(T address, U value);
+template <typename T> T getModuleBase(const std::string &moduleName);
+template <typename T> T getModuleEnd(const std::string &moduleName);
+template <typename T>
+T findIdaPatternFirst(const std::string &moduleName, const std::string &pattern);
+KittyMemory::ProcMap getModuleMap(const std::string &moduleName);
+std::string getProcessName();
+}; // namespace MemTool
