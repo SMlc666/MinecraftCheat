fb72c03bbc0a66549a3b078b5519395208042cdc
SMlc666 <qq1030452538@outlook.com>
2024-12-29 21:13:53 +0800

    完善MemTool

--- src/MemTool/MemTool.cpp
+++ src/MemTool/MemTool.cpp
@@ -6,12 +6,23 @@
 #include <unordered_map>
 static std::unordered_map<void *, bool> g_hooked_funcs;
 //NOLINTBEGIN(cppcoreguidelines-pro-type-reinterpret-cast)
+/**
+ * MemTool类提供对内存读写及其它内存操作的工具函数。
+ */
 template <typename T> bool MemTool::write(T address, const void *data, size_t size) {
   return KittyMemory::memWrite(reinterpret_cast<void *>(address), data, size);
 }
+
+/**
+ * 从指定地址读取指定大小的数据。
+ */
 template <typename T> bool MemTool::read(T address, void *data, size_t size) {
   return KittyMemory::memRead(reinterpret_cast<void *>(address), data, size);
 }
+
+/**
+ * 从指定地址读取数据并返回。
+ */
 template <typename T, typename U> T MemTool::read(U address) {
   T value;
   if (read(address, &value, sizeof(T))) {
@@ -19,12 +30,24 @@ template <typename T, typename U> T MemTool::read(U address) {
   }
   return {};
 }
+
+/**
+ * 将值写入指定地址。
+ */
 template <typename T, typename U> bool MemTool::write(T address, U value) {
   return write(address, &value, sizeof(U));
 }
+
+/**
+ * 获取当前进程的名称。
+ */
 std::string MemTool::getProcessName() {
   return KittyMemory::getProcessName();
 }
+
+/**
+ * 获取指定模块的基地址。
+ */
 template <typename T> T MemTool::getModuleBase(const std::string &moduleName) {
   KittyMemory::ProcMap Module = KittyMemory::getElfBaseMap(moduleName);
   if (Module.isUnknown()) {
@@ -32,6 +55,10 @@ template <typename T> T MemTool::getModuleBase(const std::string &moduleName) {
   }
   return reinterpret_cast<T>(Module.startAddress);
 }
+
+/**
+ * 获取指定模块的结束地址。
+ */
 template <typename T> T MemTool::getModuleEnd(const std::string &moduleName) {
   KittyMemory::ProcMap Module = KittyMemory::getElfBaseMap(moduleName);
   if (Module.isUnknown()) {
@@ -39,10 +66,18 @@ template <typename T> T MemTool::getModuleEnd(const std::string &moduleName) {
   }
   return reinterpret_cast<T>(Module.endAddress);
 }
+
+/**
+ * 获取指定模块的映射信息。
+ */
 KittyMemory::ProcMap MemTool::getModuleMap(const std::string &moduleName) {
   KittyMemory::ProcMap Module = KittyMemory::getElfBaseMap(moduleName);
   return Module;
 }
+
+/**
+ * 查找指定模块中符合给定模式的第一个地址。
+ */
 template <typename T>
 T MemTool::findIdaPatternFirst(const std::string &moduleName, const std::string &pattern) {
   KittyMemory::ProcMap Module = getModuleMap(moduleName);
@@ -52,27 +87,56 @@ T MemTool::findIdaPatternFirst(const std::string &moduleName, const std::string 
   return reinterpret_cast<T>(
       KittyScanner::findIdaPatternFirst(Module.startAddress, Module.endAddress, pattern));
 }
+
+/**
+ * 查找指定模块中的符号地址。
+ */
 template <typename T>
 T MemTool::findSymbol(const std::string *moduleName, const std::string &symbol) {
   return reinterpret_cast<T>(DobbySymbolResolver(moduleName->c_str(), symbol.c_str()));
 }
+
+/**
+ * 根据地址销毁钩子。
+ */
+template <typename T> void MemTool::destoryHookByAddress(T address) {
+  if (g_hooked_funcs.find(reinterpret_cast<void *>(address)) != g_hooked_funcs.end()) {
+    DobbyDestroy(reinterpret_cast<void *>(address));
+    g_hooked_funcs.erase(reinterpret_cast<void *>(address));
+  }
+}
+
+/**
+ * Hook类用于处理函数钩子的创建和管理。
+ */
 template <typename T>
-MemTool::Hook::Hook(T address, void *func) : hook_func(reinterpret_cast<void *>(func)) {
+MemTool::Hook::Hook(T address, void *func, void **m_orig_func, bool m_auto_destroy)
+    : hook_func(reinterpret_cast<void *>(func)), auto_destroy(m_auto_destroy) {
   if (g_hooked_funcs.find(reinterpret_cast<void *>(address)) != g_hooked_funcs.end()) {
     throw std::runtime_error("Address already hooked");
   }
-  DobbyHook(reinterpret_cast<void *>(address), func, orig_func);
+  DobbyHook(reinterpret_cast<void *>(address), func, m_orig_func);
+  orig_func = m_orig_func;
   g_hooked_funcs[reinterpret_cast<void *>(address)] = true;
 }
+
+/**
+ * 销毁钩子并清理资源。
+ */
 void MemTool::Hook::destroy() {
   if ((orig_func != nullptr) || is_destoryed || (hook_func != nullptr)) {
     return;
   }
-  DobbyDestroy(hook_func);
-  g_hooked_funcs.erase(hook_func);
+  MemTool::destoryHookByAddress(hook_func);
   is_destoryed = true;
 }
+
+/**
+ * Hook类的析构函数，自动销毁钩子（如果启用）。
+ */
 MemTool::Hook::~Hook() {
-  destroy();
+  if (auto_destroy) {
+    destroy();
+  }
 }
 //NOLINTEND(cppcoreguidelines-pro-type-reinterpret-cast)
--- src/MemTool/MemTool.hpp
+++ src/MemTool/MemTool.hpp
@@ -3,30 +3,64 @@
 #include <cstddef>
 #include <string>
 namespace MemTool {
+// 写入数据到指定地址
 template <typename T> bool write(T address, const void *data, size_t size);
+// 从指定地址读取数据
 template <typename T> bool read(T address, void *data, size_t size);
+// 从地址读取特定类型的数据
 template <typename T, typename U> T read(U address);
+// 写入特定类型的数据到地址
 template <typename T, typename U> bool write(T address, U value);
+// 获取指定模块的基地址
 template <typename T> T getModuleBase(const std::string &moduleName);
+// 获取指定模块的结束地址
 template <typename T> T getModuleEnd(const std::string &moduleName);
+// 查找模块中第一个匹配的模式
 template <typename T>
 T findIdaPatternFirst(const std::string &moduleName, const std::string &pattern);
+// 根据符号名称查找模块地址
 template <typename T> T findSymbol(const std::string *moduleName, const std::string &symbol);
+// 获取指定模块的进程映射
 KittyMemory::ProcMap getModuleMap(const std::string &moduleName);
+// 获取当前进程的名称
 std::string getProcessName();
+// 销毁指定地址的 Hook
+template <typename T> void destoryHookByAddress(T address);
+// Hook 类用于实现函数钩子
 class Hook {
 public:
-  template <typename T> Hook(T address, void *func);
+  // Hook 构造函数
+  template <typename T> Hook(T address, void *func, void **orig_func, bool m_auto_destroy = true);
+
+  // 删除复制构造函数
   Hook(const Hook &) = delete;
+
+  // 删除复制赋值运算符
   Hook &operator=(const Hook &) = delete;
+
+  // 删除移动构造函数
   Hook(Hook &&) = delete;
+
+  // 删除移动赋值运算符
   Hook &operator=(Hook &&) = delete;
+
+  // Hook 析构函数
   ~Hook();
+
+  // 释放 Hook
   void destroy();
 
 private:
+  // 是否自动销毁
+  bool auto_destroy = true;
+
+  // 是否已经被销毁
   bool is_destoryed = false;
+
+  // 原始函数指针
   void **orig_func = nullptr;
+
+  // 钩子函数指针
   void *hook_func = nullptr;
 };
 }; // namespace MemTool
