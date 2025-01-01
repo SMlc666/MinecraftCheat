ade68d440bea1202bd83e862606c6e0aa4834511
SMlc666 <qq1030452538@outlook.com>
2025-01-01 16:13:47 +0800

    refactor(signature): 优化签名初始化逻辑结构

--- src/Minecraft/signature.cpp
+++ src/Minecraft/signature.cpp
@@ -12,34 +12,32 @@ std::unordered_map<std::string, std::string> IDApattern = {
 };
 std::unordered_map<std::string, void *> signatures{};
 void signaturesInit() {
-  std::thread([]() {
-    auto start = std::chrono::system_clock::now();
-    while (true) {
-      void *base = MemTool::getModuleBase<void *>(module);
-      void *end = MemTool::getModuleEnd<void *>(module);
-      auto now = std::chrono::system_clock::now();
-      auto elaspedSeconds = now - start;
-      if (elaspedSeconds.count() > TIMEOUT_SECONDS) {
-        g_log_tool.message(LogLevel::ERROR, "initSignatures", "Timeout");
-        break;
-      }
-      if (base == nullptr || end == nullptr) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-      } else {
-        g_log_tool.message(LogLevel::INFO, "initSignatures",
-                           std::format("Module base: {:p}, end: {:p}", base, end));
-      }
+  auto start = std::chrono::system_clock::now();
+  while (true) {
+    void *base = MemTool::getModuleBase<void *>(module);
+    void *end = MemTool::getModuleEnd<void *>(module);
+    auto now = std::chrono::system_clock::now();
+    auto elaspedSeconds = now - start;
+    if (elaspedSeconds.count() > TIMEOUT_SECONDS) {
+      g_log_tool.message(LogLevel::ERROR, "initSignatures", "Timeout");
+      break;
     }
-    for (auto &item : IDApattern) {
-      void *ptr = MemTool::findIdaPatternFirst<void *>(module, item.second);
-      if (ptr == nullptr) {
-        g_log_tool.message(LogLevel::ERROR, "initSignatures",
-                           std::format("Failed to find pattern {}", item.second));
-      } else {
-        signatures[item.first] = ptr;
-        g_log_tool.message(LogLevel::INFO, "initSignatures",
-                           std::format("Found pattern {} at {:p}", item.first, ptr));
-      }
+    if (base == nullptr || end == nullptr) {
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    } else {
+      g_log_tool.message(LogLevel::INFO, "initSignatures",
+                         std::format("Module base: {:p}, end: {:p}", base, end));
     }
-  }).detach();
+  }
+  for (auto &item : IDApattern) {
+    void *ptr = MemTool::findIdaPatternFirst<void *>(module, item.second);
+    if (ptr == nullptr) {
+      g_log_tool.message(LogLevel::ERROR, "initSignatures",
+                         std::format("Failed to find pattern {}", item.second));
+    } else {
+      signatures[item.first] = ptr;
+      g_log_tool.message(LogLevel::INFO, "initSignatures",
+                         std::format("Found pattern {} at {:p}", item.first, ptr));
+    }
+  }
 }
