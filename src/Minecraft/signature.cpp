e21152682441af443ffb59803410c3c85eaea85d
SMlc666 <qq1030452538@outlook.com>
2025-01-01 16:02:10 +0800

    feat(Minecraft): 增加模块基地址和结束地址的日志记录功能

--- src/Minecraft/signature.cpp
+++ src/Minecraft/signature.cpp
@@ -1,6 +1,7 @@
 #include "signature.hpp"
 #include "MemTool.hpp"
 #include "log.hpp"
+#include <cstdint>
 #include <format>
 #include <unordered_map>
 static const std::string module = "libminecraftpe.so";
@@ -10,6 +11,10 @@ std::unordered_map<std::string, std::string> IDApattern = {
 };
 std::unordered_map<std::string, void *> signatures{};
 void signaturesInit() {
+  void *base = MemTool::getModuleBase<void *>(module);
+  void *end = MemTool::getModuleEnd<void *>(module);
+  g_log_tool.message(LogLevel::INFO, "initSignatures",
+                     std::format("Module base: {:p}, end: {:p}", base, end));
   for (auto &item : IDApattern) {
     void *ptr = MemTool::findIdaPatternFirst<void *>(module, item.second);
     if (ptr == nullptr) {
