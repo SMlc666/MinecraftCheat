39aaefa1e8cacbf5d69cdd8a7ac2cb7152706a3f
SMlc666 <qq1030452538@outlook.com>
2025-01-11 21:37:56 +0800

    feat(api): 新增脚本钩子功能实现

---
+++ src/Script/API/mem/hook.cpp
@@ -0,0 +1,12 @@
+#include "hook.hpp"
+#include "MemTool.hpp"
+ScriptAPI::Mem::Hook::ScriptHook::ScriptHook(luabridge::LuaRef _func, luabridge::LuaRef argv)
+    : m_func(_func) {
+  if (!_func.isFunction()) {
+    throw std::runtime_error("Invalid function");
+  }
+  if (!argv.isTable()) {
+    throw std::runtime_error("Invalid arguments");
+  }
+  MemTool::Hook()
+}
--- src/Script/API/mem/hook.hpp
+++ src/Script/API/mem/hook.hpp
@@ -1,1 +1,12 @@
 #pragma once
+#include "Lua/lua.hpp"
+#include "LuaBridge/detail/LuaRef.h"
+
+namespace ScriptAPI::Mem::Hook {
+class ScriptHook {
+  ScriptHook(luabridge::LuaRef _func, luabridge::LuaRef argv);
+
+private:
+  luabridge::LuaRef m_func;
+};
+} // namespace ScriptAPI::Mem::Hook
