a65e23b5ea8b884f11f1e1cf889d52199b123bff
SMlc666 <qq1030452538@outlook.com>
2025-01-12 14:31:42 +0800

    refactor(minecraft): 修改构造函数名称以符合约定

--- src/Minecraft/game/minecraft/minecraft.hpp
+++ src/Minecraft/game/minecraft/minecraft.hpp
@@ -15,7 +15,7 @@ public:
   virtual bool isOnlineClient() const;
 
 public:
-  Minecraft *_Minecraft(Minecraft *a1, void *a2, void *a3, void *a4, void *a5, void *a6, void *a7,
+  Minecraft *Minecraft_(Minecraft *a1, void *a2, void *a3, void *a4, void *a5, void *a6, void *a7,
                         void *a8, void *a9, void *a10, char a11, void *a12, void *a13, void *a14,
                         void *a15, void *a16, void *a17);
 };
