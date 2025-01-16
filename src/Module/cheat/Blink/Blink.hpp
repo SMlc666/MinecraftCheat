7883584a90c5754dd3bf00c15e06b4269c1c7ee9
SMlc666 <qq1030452538@outlook.com>
2025-01-16 13:20:35 +0800

    feat(cheat): 添加sendmsg钩子以支持blink功能

--- src/Module/cheat/Blink/Blink.hpp
+++ src/Module/cheat/Blink/Blink.hpp
@@ -4,10 +4,12 @@
 #include "menu/menu.hpp"
 #include <cstdio>
 #include <sys/socket.h>
+#include <unistd.h>
 #include <unordered_map>
 static bool is_blink = false;
 MemTool::Hook send_;
 MemTool::Hook sendto_;
+MemTool::Hook sendmsg_;
 ssize_t new_send(int sockfd, const void *buf, size_t len, int flags) {
   if (is_blink) {
     return -1;
@@ -23,6 +25,14 @@ int new_sendto(int fd, const void *buf, size_t len, int flags, const struct sock
   auto ret = sendto_.call<int>(fd, buf, len, flags, dest_addr, addrlen);
   return ret;
 }
+int new_sendmsg(int sockfd, const struct msghdr *msg, int flags) {
+  if (is_blink) {
+    return -1;
+  }
+  auto ret = sendmsg_.call<int>(sockfd, msg, flags);
+  return ret;
+}
+
 static const std::unordered_map<std::string, std::any> ConfigData = {{"enabled", false}};
 class Blink : public Module {
 public:
@@ -31,6 +41,8 @@ public:
     send_ = MemTool::Hook(send_addr, reinterpret_cast<void *>(new_send), nullptr, false);
     void *sendto_addr = MemTool::findSymbol(nullptr, "sendto");
     sendto_ = MemTool::Hook(sendto_addr, reinterpret_cast<void *>(new_sendto), nullptr, false);
+    void *sendmsg_addr = MemTool::findSymbol(nullptr, "sendmsg");
+    sendmsg_ = MemTool::Hook(sendmsg_addr, reinterpret_cast<void *>(new_sendmsg), nullptr, false);
     setOnEnable([](Module *module) { is_blink = true; });
     setOnDisable([](Module *module) { is_blink = false; });
   }
