2a73d1fedb7748fdd191c10ab849f7a0d6187beb
SMlc666 <qq1030452538@outlook.com>
2025-01-16 20:25:04 +0800

    refactor(core): 添加IClientInstance接口方法和类声明

--- src/Minecraft/game/minecraft/client/instance/Iclientinstance.hpp
+++ src/Minecraft/game/minecraft/client/instance/Iclientinstance.hpp
@@ -9,6 +9,7 @@ class Timer;
 class ClientNetworkSystem;
 class IGameModuleApp;
 class SceneFactory;
+class WeakEntityRef;
 namespace Social {
 class IUserManager;
 }
@@ -27,6 +28,7 @@ class MultiplayerServiceIdentifier;
 class FrameUpdateContext;
 class GameModuleClient;
 class BlockSource;
+class Actor;
 class LocalPlayer;
 namespace Editor {
 class IEditorPlayer;
@@ -34,6 +36,9 @@ class IEditorPlayer;
 namespace Editor {
 class IEditorManager;
 }
+struct ListenerState;
+class HitResult;
+struct LocalPlayerChangedConnector;
 class IClientInstance : Bedrock::EnableNonOwnerReferences {
 public:
   //NOLINTBEGIN
@@ -129,21 +134,98 @@ public:
   setupClientGame(bool, std::unique_ptr<GameModuleClient>) = 0;
 
   // vtable index: 29
-  virtual ::BlockSource *getRegion() = 0;
+  virtual BlockSource *getRegion() = 0;
 
   // vtable index: 30
-  virtual ::LocalPlayer *getLocalPlayer() const = 0;
+  virtual LocalPlayer *getLocalPlayer() const = 0;
 
   // vtable index: 31
   virtual void setupPrimaryClientEditorManager() = 0;
 
   // vtable index: 32
-  virtual ::Bedrock::NonOwnerPointer<Editor::IEditorPlayer> getLocalEditorPlayer() const = 0;
+  virtual Bedrock::NonOwnerPointer<Editor::IEditorPlayer> getLocalEditorPlayer() const = 0;
 
   // vtable index: 33
-  virtual ::Bedrock::NonOwnerPointer<Editor::IEditorManager> getEditorManager() const = 0;
+  virtual Bedrock::NonOwnerPointer<Editor::IEditorManager> getEditorManager() const = 0;
 
   // vtable index: 34
   virtual bool isPlayerInEditor() const = 0;
+  // vtable index: 35
+  virtual LocalPlayerChangedConnector getLocalPlayerChangedConnector() = 0;
+
+  // vtable index: 36
+  virtual WeakEntityRef getLocalUser() const = 0;
+
+  // vtable index: 37
+  virtual Actor *getCameraActor() const = 0;
+
+  // vtable index: 38
+  virtual void setCameraActor(Actor *) = 0;
+
+  // vtable index: 39
+  virtual ListenerState const *getListenerState() const = 0;
+
+  // vtable index: 40
+  virtual Actor *getCameraTargetActor() const = 0;
+
+  // vtable index: 41
+  virtual void setCameraTargetActor(::Actor *) = 0;
+
+  // vtable index: 42
+  virtual WeakEntityRef getCameraEntity() const = 0;
+
+  // vtable index: 43
+  virtual void getRawCameraEntities(WeakEntityRef &, WeakEntityRef &) const = 0;
+
+  // vtable index: 44
+  virtual void setRawCameraEntities(WeakEntityRef, WeakEntityRef) = 0;
+
+  // vtable index: 45
+  virtual HitResult const &getLatestHitResult() const = 0;
+
+  // vtable index: 46
+  virtual bool isLeavingGame() const = 0;
+
+  // vtable index: 47
+  virtual bool isDestroyingGame() const = 0;
+
+  // vtable index: 48
+  virtual bool isShuttingDown() const = 0;
+
+  // vtable index: 49
+  virtual bool useLowFrequencyUIRender() const = 0;
+
+  // vtable index: 50
+  virtual bool isSplitScreenActive() const = 0;
+
+  // vtable index: 51
+  virtual bool isVRClient() const = 0;
+
+  // vtable index: 52
+  virtual bool isARClient() const = 0;
+
+  // vtable index: 53
+  virtual bool isARVRClient() const = 0;
+
+  // vtable index: 54
+  virtual bool isHoloviewerMode() const = 0;
+
+  // vtable index: 55
+  virtual bool isHoloscreenMode() const = 0;
+
+  // vtable index: 56
+  virtual bool isLivingroomMode() const = 0;
+
+  // vtable index: 57
+  virtual bool isExitingLevel() const = 0;
+
+  // vtable index: 58
+  virtual bool isNotVLRMode() const = 0;
+
+  // vtable index: 59
+  virtual bool isHoloRealityMode() const = 0;
+
+  // vtable index: 60
+  virtual bool isRealityFullVRMode() const = 0;
   //NOLINTEND
 };
