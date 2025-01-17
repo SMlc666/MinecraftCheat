93fb779c7b38eb702c19894ae7bf17fe1d445a53
SMlc666 <qq1030452538@outlook.com>
2025-01-17 19:55:56 +0800

    refactor: 更新钩子函数并修改签名表

--- src/Minecraft/hooks/hooks.cpp
+++ src/Minecraft/hooks/hooks.cpp
@@ -7,22 +7,16 @@
 #include <format>
 #include <string>
 class ClientInstance;
-MemTool::Hook ClientInstance_ClientInstance_;
-
-ClientInstance *ClientInstance_ClientInstance(ClientInstance *mClientInstance, int a2, int a3,
-                                              int a4, char a5, void *a6, void *a7, int a8,
-                                              void *a9) {
-  auto *ret = ClientInstance_ClientInstance_.call<ClientInstance *>(mClientInstance, a2, a3, a4, a5,
-                                                                    a6, a7, a8, a9);
-  runtimes::setClientInstance(mClientInstance);
-  g_log_tool.message(
-      LogLevel::INFO, "ClientInstance_ClientInstance",
-      std::format("ClientInstance: {:p}", reinterpret_cast<void *>(mClientInstance)));
-  return ret;
+MemTool::Hook ClientInstance_onStartJoinGame_;
+void ClientInstance_onStartJoinGame(ClientInstance *self, bool a1, std::string a2, void *a3) {
+  ClientInstance_onStartJoinGame_.call<void>(self, a1, a2, a3);
+  g_log_tool.message(LogLevel::INFO, "ClientInstance_onStartJoinGame",
+                     std::format("ClientInstance::onStartJoinGame({:p},{}, {}, {})",
+                                 reinterpret_cast<void *>(self), a1, a2, a3));
 }
 void hooksInit() {
-  void *clientInstance = getSign<void *>("ClientInstance::ClientInstance");
-  ClientInstance_ClientInstance_ = MemTool::Hook(
-      clientInstance, reinterpret_cast<void *>(ClientInstance_ClientInstance), nullptr, false);
+  void *clientInstance = getSign<void *>("ClientInstance::onStartJoinGame_");
+  ClientInstance_onStartJoinGame_ = MemTool::Hook(
+      clientInstance, reinterpret_cast<void *>(ClientInstance_onStartJoinGame), nullptr, false);
   g_log_tool.message(LogLevel::INFO, "HooksInit", "Hooks inited");
 }
--- src/Minecraft/signature.cpp
+++ src/Minecraft/signature.cpp
@@ -9,8 +9,7 @@ static const std::string module = "libminecraftpe.so";
 std::unordered_map<std::string, uintptr_t> IDApattern = {
     {"Player::Player", 0x6DD37FC},       {"Minecraft::Minecraft", 0x7493638},
     {"Minecraft::update", 0x749472C},    {"ClientInstance::ClientInstance", 0x509BE10},
-    {"Dimension::Dimension", 0x8A4E578},
-};
+    {"Dimension::Dimension", 0x8A4E578}, {"ClientInstance::onStartJoinGame", 0x50A2A48}};
 //NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 std::unordered_map<std::string, void *> signatures{};
 void signaturesInit() {
