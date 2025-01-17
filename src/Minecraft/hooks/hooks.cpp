9f53c7b2d6ef32d6c938f3443c95535d722582e1
SMlc666 <qq1030452538@outlook.com>
2025-01-17 18:24:21 +0800

    refactor(core): 更新Minecraft_update函数参数类型并添加ClientInstance初始化逻辑

--- src/Minecraft/hooks/hooks.cpp
+++ src/Minecraft/hooks/hooks.cpp
@@ -1,6 +1,5 @@
 #include "hooks.hpp"
 #include "MemTool.hpp"
-#include "game/minecraft/client/instance/clientinstance.hpp"
 #include "log.hpp"
 #include "runtimes/runtimes.hpp"
 #include "signature.hpp"
@@ -12,12 +11,22 @@ class ClientInstance;
 class Minecraft;
 MemTool::Hook Minecraft_update_;
 MemTool::Hook ClientInstance_ClientInstance_;
-bool Minecraft_update(void *Minecraft) {
-  bool ret = Minecraft_update_.call<bool>(Minecraft);
+bool Minecraft_update(Minecraft *mMinecraft) {
+  bool ret = Minecraft_update_.call<bool>(mMinecraft);
   ModuleManager::tickAllModules();
   return ret;
 }
-
+ClientInstance *ClientInstance_ClientInstance(ClientInstance *mClientInstance, int a2, int a3,
+                                              int a4, char a5, void *a6, void *a7, int a8,
+                                              void *a9) {
+  auto *ret = ClientInstance_ClientInstance_.call<ClientInstance *>(mClientInstance, a2, a3, a4, a5,
+                                                                    a6, a7, a8, a9);
+  runtimes::setClientInstance(mClientInstance);
+  g_log_tool.message(
+      LogLevel::INFO, "ClientInstance_ClientInstance",
+      std::format("ClientInstance: {:p}", reinterpret_cast<void *>(mClientInstance)));
+  return ret;
+}
 void hooksInit() {
   void *update = getSign<void *>("Minecraft::update");
   Minecraft_update_ =
