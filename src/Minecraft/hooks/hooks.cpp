a289e30efbf4bd5596e5b17ebf2053c685417b0e
SMlc666 <qq1030452538@outlook.com>
2025-01-17 18:33:37 +0800

    feat: 添加ClientInstance构造函数的hook

--- src/Minecraft/hooks/hooks.cpp
+++ src/Minecraft/hooks/hooks.cpp
@@ -31,5 +31,8 @@ void hooksInit() {
   void *update = getSign<void *>("Minecraft::update");
   Minecraft_update_ =
       MemTool::Hook(update, reinterpret_cast<void *>(Minecraft_update), nullptr, false);
+  void *clientInstance = getSign<void *>("ClientInstance::ClientInstance");
+  ClientInstance_ClientInstance_ = MemTool::Hook(
+      clientInstance, reinterpret_cast<void *>(ClientInstance_ClientInstance), nullptr, false);
   g_log_tool.message(LogLevel::INFO, "HooksInit", "Hooks inited");
 }
