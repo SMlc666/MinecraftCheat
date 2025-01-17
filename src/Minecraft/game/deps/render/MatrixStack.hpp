cbea98c72cb07c8f998cc110dddbbff1dd8a0d2a
SMlc666 <qq1030452538@outlook.com>
2025-01-17 16:53:08 +0800

    feat(api): 添加MatrixStack类定义和actor相关头文件

--- src/Minecraft/game/deps/render/MatrixStack.hpp
+++ src/Minecraft/game/deps/render/MatrixStack.hpp
@@ -1,3 +1,22 @@
 #pragma once
+#include "base/alias.hpp"
+//NOLINTBEGIN
+class MatrixStack {
+public:
+  struct MatrixStackRef {
+  public:
+    alias::UntypedStorage<8, 8> mUnka8f7f2;
+    alias::UntypedStorage<8, 8> mUnk88f465;
 
-class MatrixStack {};
+  public:
+    MatrixStackRef &operator=(MatrixStackRef const &);
+    MatrixStackRef(MatrixStackRef const &);
+    MatrixStackRef();
+  };
+
+public:
+  MatrixStack &operator=(MatrixStack const &);
+  MatrixStack(MatrixStack const &);
+  MatrixStack();
+};
+//NOLINTEND
