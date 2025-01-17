685c185cd9926baf7ecbd0381a67b1bdef153867
SMlc666 <qq1030452538@outlook.com>
2025-01-17 22:53:49 +0800

    feat: 添加SpinAttack模块

--- src/Module/cheat/cheatsetup.cpp
+++ src/Module/cheat/cheatsetup.cpp
@@ -1,6 +1,8 @@
 #include "cheatsetup.hpp"
 #include "ModuleManager.hpp"
 #include "Blink/Blink.hpp"
+#include "SpinAttack/SpinAttack.hpp"
 void cheatSetup() {
   ModuleManager::addModule(new cheat::Blink);
+  ModuleManager::addModule(new cheat::SpinAttack);
 }
