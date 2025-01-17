9b3e00645228f3de1ef8f5de4ed2186f66734b60
SMlc666 <qq1030452538@outlook.com>
2025-01-17 18:06:36 +0800

    refactor: 移除Minecraft实例的静态变量和相关方法

--- src/Minecraft/runtimes/runtimes.cpp
+++ src/Minecraft/runtimes/runtimes.cpp
@@ -1,8 +1,1 @@
 #include "runtimes.hpp"
-static Minecraft *g_mc = nullptr;
-Minecraft *runtimes::getMinecraftInstance() {
-  return g_mc;
-}
-void runtimes::setMinecraftInstance(Minecraft *mc_) {
-  g_mc = mc_;
-}
