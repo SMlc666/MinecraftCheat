ad66ead21dad1c1368d9ee1661c10a929f81b420
SMlc666 <qq1030452538@outlook.com>
2025-01-18 19:15:52 +0800

    fix(ui): 修复getLocalPlayer函数vtable索引错误并添加调试日志

--- src/Minecraft/game/minecraft/client/instance/clientinstance.cpp
+++ src/Minecraft/game/minecraft/client/instance/clientinstance.cpp
@@ -1,9 +1,14 @@
 #include "clientinstance.hpp"
 #include "game/minecraft/actor/player/localplayer.hpp"
+#include "log.hpp"
+#include <format>
 //NOLINTBEGIN
 LocalPlayer *ClientInstance::getLocalPlayer() const {
   using function = LocalPlayer *(*)(const ClientInstance *);
-  auto func = reinterpret_cast<function>(this->vtable[31]);
+  g_log_tool.message(
+      LogLevel::DEBUG, "ClientInstance::getLocalPlayer",
+      std::format("vtable[33] = {:p}", reinterpret_cast<void *>(this->vtable[33]))) auto func =
+      reinterpret_cast<function>(this->vtable[33]);
   return func(this);
 }
 
