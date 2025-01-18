a92cc65b111afb636c613a486e10ce6989b5b210
SMlc666 <qq1030452538@outlook.com>
2025-01-18 08:32:40 +0800

    feat: 添加获取本地玩家的调试功能

--- src/Module/menu/menus/Debug.hpp
+++ src/Module/menu/menus/Debug.hpp
@@ -1,5 +1,8 @@
 #pragma once
+#include "game/minecraft/client/instance/clientinstance.hpp"
 #include "imgui/imgui.h"
+#include "runtimes/runtimes.hpp"
+#include "log.hpp"
 #include <cstdlib>
 #include <stdexcept>
 void drawDebugMenu() {
@@ -21,5 +24,19 @@ void drawDebugMenu() {
       *(int *)0 = 0; //NOLINT
     }
     ImGui::TreePop();
+    if (ImGui::Button("get localplayer")) {
+      ClientInstance *instance = runtimes::getClientInstance();
+      if (instance != nullptr) {
+        auto *player = instance->getLocalPlayer();
+        if (player != nullptr) {
+          g_log_tool.message(LogLevel::DEBUG, "Debug",
+                             std::format("LocalPlayer: {:p}", reinterpret_cast<void *>(player)));
+        } else {
+          g_log_tool.message(LogLevel::DEBUG, "Debug", "LocalPlayer: nullptr");
+        }
+      } else {
+        g_log_tool.message(LogLevel::DEBUG, "Debug", "ClientInstance: nullptr");
+      }
+    }
   }
 }
