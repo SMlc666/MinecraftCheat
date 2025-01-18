cdb4a96d16e39f916e52a21f7f93155ab5aaa31e
SMlc666 <qq1030452538@outlook.com>
2025-01-18 19:26:58 +0800

    style: 移除调试日志打印

--- src/Minecraft/game/minecraft/client/instance/clientinstance.cpp
+++ src/Minecraft/game/minecraft/client/instance/clientinstance.cpp
@@ -5,8 +5,6 @@
 //NOLINTBEGIN
 LocalPlayer *ClientInstance::getLocalPlayer() const {
   using function = LocalPlayer *(*)(const ClientInstance *);
-  g_log_tool.message(LogLevel::DEBUG, "ClientInstance::getLocalPlayer",
-                     std::format("vtable[33] = {:p}", reinterpret_cast<void *>(this->vtable[33])));
   auto func = reinterpret_cast<function>(this->vtable[33]);
   return func(this);
 }
