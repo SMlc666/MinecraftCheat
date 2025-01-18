520a37112fa5fb5b8dcbdde83ddd14cfdfa3c087
SMlc666 <qq1030452538@outlook.com>
2025-01-18 19:37:29 +0800

    feat(cheat): 添加闪烁功能模块

---
+++ src/Module/cheat/Blink/Blink.cpp
@@ -0,0 +1,24 @@
+#include "Blink.hpp"
+#include "Module.hpp"
+#include "menu/menu.hpp"
+#include <cstddef>
+#include <sys/socket.h>
+#include <unordered_map>
+#include "MemTool.hpp"
+static bool isBlink;
+static MemTool::Hook send_;
+static int Blink_send(int fd, const void *buf, size_t n, int flags) {
+  if (isBlink) {
+    return 0;
+  }
+  return send_.call<int>(fd, buf, n, flags);
+}
+static std::unordered_map<std::string, std::any> ConfigData = {
+    {"enabled", false},
+};
+Blink::Blink() : Module("Blink", MenuType::COMBAT_MENU, ConfigData) {
+  send_ = MemTool::Hook(MemTool::findSymbol(nullptr, "send"), reinterpret_cast<void *>(Blink_send),
+                        nullptr, false);
+  setOnEnable([](Module *module) { isBlink = true; });
+  setOnDisable([](Module *module) { isBlink = false; });
+}
---
+++ src/Module/cheat/Blink/Blink.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Module/Module.hpp"
+
+class Blink : public Module {
+public:
+  Blink();
+};
