07abd8fffc51bc150e392f29cf710825da71d1f8
SMlc666 <qq1030452538@outlook.com>
2025-01-18 11:52:57 +0800

    feat: 添加玩家名称获取函数

--- src/Minecraft/game/minecraft/actor/player/player.hpp
+++ src/Minecraft/game/minecraft/actor/player/player.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include "game/minecraft/actor/mob/mob.hpp"
+#include <string>
 class GameMode;
 class Player : public Mob {
 public:
   [[nodiscard]] GameMode &getGameMode() const;
+  [[nodiscard]] std::string getName() const;
 };
