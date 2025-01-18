ba111347bef98c4133a21141a569028335030f9e
SMlc666 <qq1030452538@outlook.com>
2025-01-18 19:38:51 +0800

    feat(cheat): 添加Blink模块

--- src/Module/cheat/cheatsetup.cpp
+++ src/Module/cheat/cheatsetup.cpp
@@ -1,6 +1,8 @@
 #include "cheatsetup.hpp"
 #include "ModuleManager.hpp"
 #include "SpinAttack/SpinAttack.hpp"
+#include "Blink/Blink.hpp"
 void cheatSetup() {
   ModuleManager::addModule(new cheat::SpinAttack);
+  ModuleManager::addModule(new cheat::Blink);
 }
