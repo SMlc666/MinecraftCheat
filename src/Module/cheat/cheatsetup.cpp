a188b0597e591ef17e7af732631de6bc1a34a91e
SMlc666 <qq1030452538@outlook.com>
2025-01-18 19:28:35 +0800

    refactor(cheat): 删除Blink模块相关代码

--- src/Module/cheat/cheatsetup.cpp
+++ src/Module/cheat/cheatsetup.cpp
@@ -1,8 +1,6 @@
 #include "cheatsetup.hpp"
 #include "ModuleManager.hpp"
-#include "Blink/Blink.hpp"
 #include "SpinAttack/SpinAttack.hpp"
 void cheatSetup() {
-  ModuleManager::addModule(new cheat::Blink);
   ModuleManager::addModule(new cheat::SpinAttack);
 }
