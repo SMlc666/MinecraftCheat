28b40300078f73de46e6626404c5ed0e22e5d89e
SMlc666 <qq1030452538@outlook.com>
2025-01-18 09:19:34 +0800

    fix: 修正getLocalPlayer函数索引错误

--- src/Minecraft/game/minecraft/client/instance/clientinstance.cpp
+++ src/Minecraft/game/minecraft/client/instance/clientinstance.cpp
@@ -3,7 +3,7 @@
 //NOLINTBEGIN
 LocalPlayer *ClientInstance::getLocalPlayer() const {
   using function = LocalPlayer *(*)(const ClientInstance *);
-  auto func = reinterpret_cast<function>(this->vtable[29]);
+  auto func = reinterpret_cast<function>(this->vtable[30]);
   return func(this);
 }
 //NOLINTEND
