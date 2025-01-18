615a295c8c00ab5fc28d3dd443eff21c7eb05070
SMlc666 <qq1030452538@outlook.com>
2025-01-18 08:47:52 +0800

    fix: 修复setClientInstance函数参数名错误

--- src/Minecraft/runtimes/runtimes.cpp
+++ src/Minecraft/runtimes/runtimes.cpp
@@ -4,6 +4,6 @@ static ClientInstance *instance = nullptr;
 ClientInstance *runtimes::getClientInstance() {
   return instance;
 }
-void runtimes::setClientInstance(ClientInstance *instance) {
-  instance = instance;
+void runtimes::setClientInstance(ClientInstance *minstance) {
+  instance = minstance;
 }
