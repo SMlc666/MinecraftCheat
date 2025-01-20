ad7844516fd1e79fe2cc4f901643df3ea512c221
SMlc666 <qq1030452538@outlook.com>
2025-01-20 16:56:14 +0800

    refactor(clientinstance): 添加Minecraft指针并调整偏移量

--- src/Minecraft/game/minecraft/client/instance/clientinstance.hpp
+++ src/Minecraft/game/minecraft/client/instance/clientinstance.hpp
@@ -1,10 +1,15 @@
 #pragma once
 #include "game/minecraft/actor/player/localplayer.hpp"
 #include <cstdint>
+class Minecraft;
 class ClientInstance {
 public:
   uintptr_t **vtable;
-
+  std::byte placeholder[176];
+  Minecraft *minecraftPtr; // 将偏移量44处的4字节改为Minecraft*指针
 public:
   [[nodiscard]] LocalPlayer *getLocalPlayer() const;
 };
+
+static_assert(offsetof(ClientInstance, minecraftPtr) == 0xB0 + 8,
+              "Minecraft pointer offset is wrong");
