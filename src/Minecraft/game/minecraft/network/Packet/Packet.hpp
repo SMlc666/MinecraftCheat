e9d62e10a9fd09b3ddac16e4b5808dfb06eed1fb
SMlc666 <qq1030452538@outlook.com>
2025-01-21 21:42:41 +0800

    feat: 添加Packet基类定义

---
+++ src/Minecraft/game/minecraft/network/Packet/Packet.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "game/deps/core/Bedrock/Result.hpp"
+#include "game/minecraft/network/Packet/MinecraftPacketIds.hpp"
+#include <string>
+class ReadOnlyBinaryStream;
+class BinaryStream;
+class Packet {
+  virtual ~Packet();
+  virtual MinecraftPacketIds getId() const = 0;
+  virtual std::string getName() const = 0;
+  virtual void write(BinaryStream &stream) const = 0;
+  virtual Bedrock::Result<void, std::error_code> read(ReadOnlyBinaryStream &bitStream);
+  virtual bool disallowBatching() const;
+  virtual bool isValid() const;
+  virtual Bedrock::Result<void, std::error_code> _read(ReadOnlyBinaryStream &stream) = 0;
+};
