48539117625045f94d340994e0ca95394e2735d9
SMlc666 <qq1030452538@outlook.com>
2025-01-21 22:02:49 +0800

    feat(cheat): 添加网络模块

--- src/Module/cheat/cheatsetup.cpp
+++ src/Module/cheat/cheatsetup.cpp
@@ -2,9 +2,11 @@
 #include "ModuleManager.hpp"
 #include "SpinAttack/SpinAttack.hpp"
 #include "Blink/Blink.hpp"
+#include "cheat/Network/Network.hpp"
 #include "cheat/Timer/Timer.hpp"
 void cheatSetup() {
   ModuleManager::addModule(new cheat::SpinAttack);
   ModuleManager::addModule(new cheat::Blink);
   ModuleManager::addModule(new cheat::Timer);
+  ModuleManager::addModule(new cheat::Network);
 }
