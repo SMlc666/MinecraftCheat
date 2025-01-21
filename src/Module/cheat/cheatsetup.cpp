b41c00db29b4ead4d70fdab482bef37f4de2a1ea
SMlc666 <qq1030452538@outlook.com>
2025-01-21 14:24:59 +0800

    feat: 添加Timer模块到cheatSetup函数

--- src/Module/cheat/cheatsetup.cpp
+++ src/Module/cheat/cheatsetup.cpp
@@ -2,7 +2,9 @@
 #include "ModuleManager.hpp"
 #include "SpinAttack/SpinAttack.hpp"
 #include "Blink/Blink.hpp"
+#include "cheat/Timer/Timer.hpp"
 void cheatSetup() {
   ModuleManager::addModule(new cheat::SpinAttack);
   ModuleManager::addModule(new cheat::Blink);
+  ModuleManager::addModule(new cheat::Timer);
 }
