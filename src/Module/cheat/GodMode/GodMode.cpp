88f3d621395a49e3f9ac72be3d5a90cd3105f24b
SMlc666 <qq1030452538@outlook.com>
2025-01-22 13:13:56 +0800

    feat: 新增GodMode作弊模块

---
+++ src/Module/cheat/GodMode/GodMode.cpp
@@ -0,0 +1,30 @@
+#include "GodMode.hpp"
+#include "Module.hpp"
+#include "game/minecraft/actor/player/gamemode/gamemode.hpp"
+#include "game/minecraft/actor/player/localplayer.hpp"
+#include "game/minecraft/client/instance/clientinstance.hpp"
+#include "menu/menu.hpp"
+#include "runtimes/runtimes.hpp"
+#include <unordered_map>
+static const std::unordered_map<std::string, std::any> ConfigData = {
+    {"enabled", false},
+};
+cheat::GodMode::GodMode() : Module("GodMode", MenuType::COMBAT_MENU, ConfigData) {
+  setOnEnable([](Module *module) {});
+  setOnDisable([](Module *module) {});
+  setOnTick([](Module *module) {
+    if (!module->getGUI().Get<bool>("enabled")) {
+      return;
+    }
+    ClientInstance *client = runtimes::getClientInstance();
+    if (!client) {
+      return;
+    }
+    LocalPlayer *player = client->getLocalPlayer();
+    if (!player) {
+      return;
+    }
+    GameMode gamemode = player->getGameMode();
+    gamemode.attack(*player);
+  });
+}
---
+++ src/Module/cheat/GodMode/GodMode.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "Module.hpp"
+namespace cheat {
+class GodMode : public Module {
+public:
+  GodMode();
+};
+} // namespace cheat
