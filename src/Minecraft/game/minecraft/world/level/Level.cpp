ac3941e21df7aeaac4d76b7af93b0ffbf0bbca06
SMlc666 <qq1030452538@outlook.com>
2025-01-22 13:28:02 +0800

    fix: 修正forEachPlayer函数中vtable的访问方式

--- src/Minecraft/game/minecraft/world/level/Level.cpp
+++ src/Minecraft/game/minecraft/world/level/Level.cpp
@@ -1,6 +1,6 @@
 #include "Level.hpp"
 void Level::forEachPlayer(std::function<bool(Player &)> *callback) {
   using function = void (*)(Level *, std::function<bool(Player &)> *);
-  auto func = reinterpret_cast<function>(this->vtable[252]);
-  return func(this, callback);
+  auto func = reinterpret_cast<function *>((reinterpret_cast<char *>(this) + 0x7E0LL));
+  return (*func)(this, callback);
 }
