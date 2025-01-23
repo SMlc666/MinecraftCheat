893c1829f4d79445f98b926a600be4122fc43b5d
SMlc666 <qq1030452538@outlook.com>
2025-01-23 22:22:51 +0800

    refactor(actor): 调整Actor类成员变量和填充字节数

--- src/Minecraft/game/minecraft/actor/actor.hpp
+++ src/Minecraft/game/minecraft/actor/actor.hpp
@@ -1,20 +1,23 @@
 #pragma once
 #include "game/common/gamerefs/WeakRef.hpp"
+#include "game/minecraft/world/level/BlockSourceListener.hpp"
 #include <cstddef>
 #include <memory>
 class Dimension;
 class Level;
+class BlockSource;
 #include <cstdint>
 class Actor {
 public:
   uintptr_t **vtable;
-  std::byte padding8[0x2D0];
+  std::byte padding8[0x2C0];
   WeakRef<Dimension> mDimension;
+  BlockSource &mRegion;
 
 public:
   [[nodiscard]] Level *getLevel() const;
   [[nodiscard]] Dimension *getDimension() const;
   [[nodiscard]] bool hasDimension() const;
 }; // namespace class Actor
-static_assert(offsetof(Actor, mDimension) == 0x2D8);
-static_assert(sizeof(std::weak_ptr<std::shared_ptr<Dimension>>) == 16);
+static_assert(offsetof(Actor, mRegion) == 0x2D8);
+static_assert(sizeof(WeakRef<Dimension>) == 16);
