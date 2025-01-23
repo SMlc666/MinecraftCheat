408abb09807f262e5e4cff62e3ecd0108792aa65
SMlc666 <qq1030452538@outlook.com>
2025-01-23 10:23:18 +0800

    feat: 添加ClickGUI模块到cheatSetup函数

--- src/Module/cheat/cheatsetup.cpp
+++ src/Module/cheat/cheatsetup.cpp
@@ -2,6 +2,7 @@
 #include "ModuleManager.hpp"
 #include "SpinAttack/SpinAttack.hpp"
 #include "Blink/Blink.hpp"
+#include "cheat/ClickGUI/ClickGUI.hpp"
 #include "cheat/GodMode/GodMode.hpp"
 #include "cheat/Network/Network.hpp"
 #include "cheat/Timer/Timer.hpp"
@@ -11,4 +12,5 @@ void cheatSetup() {
   ModuleManager::addModule(new cheat::Timer);
   ModuleManager::addModule(new cheat::Network);
   ModuleManager::addModule(new cheat::GodMode);
+  ModuleManager::addModule(new cheat::ClickGUI);
 }
