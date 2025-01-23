bbd8b13c51f14c96bf831b7a9dd936b0fd4ab2f6
SMlc666 <qq1030452538@outlook.com>
2025-01-23 10:22:58 +0800

    feat(core): 添加ClickGUI模块

---
+++ src/Module/cheat/ClickGUI/ClickGUI.cpp
@@ -0,0 +1,24 @@
+#include "ClickGUI.hpp"
+#include "imgui/imgui.h"
+#include "menu/menu.hpp"
+#include <unordered_map>
+#include "Module.hpp"
+#include <any>
+#include <string>
+static const std::unordered_map<std::string, std::any> ConfigData = {{"enabled", true},
+                                                                     {"WindowRounding", 10.0F}};
+cheat::ClickGUI::ClickGUI() : Module("ClickGUI", MenuType::RENDER_MENU, ConfigData) {
+  setOnDisable([](Module *module) {
+
+  });
+  setOnEnable([](Module *module) {
+
+  });
+  setOnDrawGUI([](Module *module) {
+    module->getGUI().SliderFloat("WindowRounding", "窗口圆角", 0.0F, 10.0F,
+                                 [](float value) { ImGui::GetStyle().WindowRounding = value; });
+  });
+  setOnLoad([](Module *module) {
+    ImGui::GetStyle().WindowRounding = module->getGUI().Get<float>("WindowRounding");
+  });
+}
---
+++ src/Module/cheat/ClickGUI/ClickGUI.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "Module.hpp"
+namespace cheat {
+class ClickGUI : public Module {
+public:
+  ClickGUI();
+};
+} // namespace cheat
