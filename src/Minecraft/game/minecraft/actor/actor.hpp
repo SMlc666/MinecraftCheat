415652f798be53258d26e5b0ee7fc55d67976a35
SMlc666 <qq1030452538@outlook.com>
2025-01-23 15:55:31 +0800

    refactor(actor): 更新Actor类结构和方法

--- src/Minecraft/game/minecraft/actor/actor.hpp
+++ src/Minecraft/game/minecraft/actor/actor.hpp
@@ -1,12 +1,18 @@
 #pragma once
+#include <cstddef>
+#include <memory>
 class Dimension;
 class Level;
 #include <cstdint>
 class Actor {
 public:
   uintptr_t **vtable;
+  std::byte padding8[0x2D0];
+  std::weak_ptr<Dimension> mDimension;
 
 public:
   [[nodiscard]] Level *getLevel() const;
-  [[nodiscard]] Dimension *getDimension() const;
+  [[nodiscard]] Dimension &getDimension() const;
+  [[nodiscard]] bool hasDimension() const;
 }; // namespace class Actor
+static_assert(offsetof(Actor, mDimension) == 0x2D8);
