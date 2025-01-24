d526c984952d87961b55de109b31d90bffbf8835
SMlc666 <qq1030452538@outlook.com>
2025-01-24 16:34:06 +0800

    feat: 添加快捷方式绘制功能

---
+++ src/Draw/Module/shortcut.cpp
@@ -0,0 +1,32 @@
+#include "shortcut.hpp"
+#include "ModuleManager.hpp"
+#include "imgui/imgui.h"
+#include <unordered_map>
+ImVec4 disableBackgroundColor = ImVec4(0.0F / 255.0F, 255.0F / 255.0F, 207.0F / 255.0F, 1.0F);
+ImVec4 enableBackgroundColor = ImVec4(0.0F / 255.0F, 163.0F / 255.0F, 255.0F / 255.0F, 1.0F);
+ImVec4 disableTextColor = ImVec4(0.0F / 255.0F, 0.0F / 255.0F, 0.0F / 255.0F, 1.0F);
+ImVec4 enableTextColor = ImVec4(0.0F / 255.0F, 0.0F / 255.0F, 0.0F / 255.0F, 1.0F);
+ImVec2 shortcutSize = ImVec2(100.0F, 20.0F);
+void drawAllShortcuts() {
+  auto mModules = ModuleManager::getModules();
+  for (auto const &value : mModules) {
+    auto const &m = value.second;
+    auto &gui = m->getGUI();
+    if (gui.Has("shortcut") && gui.Get<bool>("shortcut")) {
+      bool mEnabled = gui.Has("enabled") && gui.Get<bool>("enabled");
+      ImGui::Begin(m->getName().c_str(), nullptr,
+                   ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
+                       ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoScrollbar);
+      if (mEnabled) {
+        ImGui::PushStyleColor(ImGuiCol_Button, enableBackgroundColor);
+        ImGui::PushStyleColor(ImGuiCol_Text, enableTextColor);
+      } else {
+        ImGui::PushStyleColor(ImGuiCol_Button, disableBackgroundColor);
+        ImGui::PushStyleColor(ImGuiCol_Text, disableTextColor);
+      }
+      ImGui::Button(m->getName().c_str(), shortcutSize);
+      ImGui::PopStyleColor(2);
+      ImGui::End();
+    }
+  }
+}
---
+++ src/Draw/Module/shortcut.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "imgui/imgui.h"
+extern ImVec4 enableBackgroundColor;
+extern ImVec4 disableBackgroundColor;
+extern ImVec4 enableTextColor;
+extern ImVec4 disableTextColor;
+extern ImVec2 shortcutSize;
+void drawAllShortcuts();
