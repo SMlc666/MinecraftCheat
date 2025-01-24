562457fa7412908a50738e52c4258ba55e0a2655
SMlc666 <qq1030452538@outlook.com>
2025-01-24 16:48:51 +0800

    feat(shortcut): 添加长按快捷键功能

--- src/Draw/Module/shortcut.cpp
+++ src/Draw/Module/shortcut.cpp
@@ -2,11 +2,16 @@
 #include "ModuleManager.hpp"
 #include "imgui/imgui.h"
 #include <unordered_map>
+struct holdInfo {
+  double pressTime = 0.0;
+  double holdDuration = 0.0;
+};
 ImVec4 disableBackgroundColor = ImVec4(0.0F / 255.0F, 255.0F / 255.0F, 207.0F / 255.0F, 1.0F);
 ImVec4 enableBackgroundColor = ImVec4(0.0F / 255.0F, 163.0F / 255.0F, 255.0F / 255.0F, 1.0F);
 ImVec4 disableTextColor = ImVec4(0.0F / 255.0F, 0.0F / 255.0F, 0.0F / 255.0F, 1.0F);
 ImVec4 enableTextColor = ImVec4(0.0F / 255.0F, 0.0F / 255.0F, 0.0F / 255.0F, 1.0F);
 ImVec2 shortcutSize = ImVec2(100.0F, 20.0F);
+static std::unordered_map<std::string, holdInfo> shortcutHoldInfo = {};
 void drawAllShortcuts() {
   auto mModules = ModuleManager::getModules();
   for (auto &value : mModules) {
@@ -25,12 +30,21 @@ void drawAllShortcuts() {
         ImGui::PushStyleColor(ImGuiCol_Text, disableTextColor);
       }
       if (ImGui::Button(m->getName().c_str(), shortcutSize)) {
+        shortcutHoldInfo[m->getName()].pressTime = ImGui::GetTime();
         if (mEnabled) {
           ModuleManager::disableModule(m);
           gui.Set("enabled", false);
         } else {
           ModuleManager::enableModule(m);
           gui.Set("enabled", true);
+        }
+      }
+      if (ImGui::IsItemActive()) {
+        shortcutHoldInfo[m->getName()].holdDuration =
+            ImGui::GetTime() - shortcutHoldInfo[m->getName()].pressTime;
+        if (shortcutHoldInfo[m->getName()].holdDuration > 1.0) {
+          ImGui::SetWindowPos(ImGui::GetMousePos(), ImGuiCond_Always);
+          ImGui::SetWindowFocus();
         }
       }
       ImGui::PopStyleColor(2);
