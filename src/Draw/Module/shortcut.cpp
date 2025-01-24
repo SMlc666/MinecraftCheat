f989ff9f3c8473a2d75925bb8735619acd5ec6fd
SMlc666 <qq1030452538@outlook.com>
2025-01-24 20:51:01 +0800

    feat(ui): 添加快捷方式的内边距和文本缩放参数

--- src/Draw/Module/shortcut.cpp
+++ src/Draw/Module/shortcut.cpp
@@ -7,6 +7,8 @@ ImVec4 enableBackgroundColor = ImVec4(0.0F / 255.0F, 163.0F / 255.0F, 255.0F / 2
 ImVec4 disableTextColor = ImVec4(0.0F / 255.0F, 0.0F / 255.0F, 0.0F / 255.0F, 1.0F);
 ImVec4 enableTextColor = ImVec4(0.0F / 255.0F, 0.0F / 255.0F, 0.0F / 255.0F, 1.0F);
 ImVec2 shortcutSize = ImVec2(200.0F, 100.0F);
+ImVec2 shortcutPadding = ImVec2(0.0F, 0.0F);
+float shortcutTextScale = 1.0F;
 float shortcutRouding = 5.0F;
 void drawAllShortcuts() {
   auto mModules = ModuleManager::getModules();
@@ -29,6 +31,8 @@ void drawAllShortcuts() {
         ImGui::PushStyleColor(ImGuiCol_ButtonActive, disableBackgroundColor);
       }
       ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, shortcutRouding);
+      ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, shortcutPadding);
+      ImGui::SetWindowFontScale(shortcutTextScale);
       if (ImGui::Button(m->getName().c_str(), shortcutSize)) {
         if (mEnabled) {
           ModuleManager::disableModule(m);
@@ -38,8 +42,9 @@ void drawAllShortcuts() {
           gui.Set("enabled", true);
         }
       }
+      ImGui::SetWindowFontScale(1.0F);
       ImGui::PopStyleColor(3);
-      ImGui::PopStyleVar();
+      ImGui::PopStyleVar(2);
       ImGui::End();
     }
   }
--- src/Draw/Module/shortcut.hpp
+++ src/Draw/Module/shortcut.hpp
@@ -5,5 +5,7 @@ extern ImVec4 disableBackgroundColor;
 extern ImVec4 enableTextColor;
 extern ImVec4 disableTextColor;
 extern ImVec2 shortcutSize;
+extern ImVec2 shortcutPadding;
 extern float shortcutRouding;
+extern float shortcutTextScale;
 void drawAllShortcuts();
