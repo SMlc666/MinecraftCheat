20993c51a8b5731a836924f31b91ea8c204cf5a9
SMlc666 <qq1030452538@outlook.com>
2025-01-25 20:58:32 +0800

    refactor(world): 重构 forEachPlayer 函数参数

--- src/Minecraft/game/minecraft/world/level/dimension/dimension.cpp
+++ src/Minecraft/game/minecraft/world/level/dimension/dimension.cpp
@@ -1,7 +1,9 @@
 #include "dimension.hpp"
+
+#include <utility>
 #include "signature.hpp"
-void Dimension::forEachPlayer(std::function<bool(Player &)> &callback) {
-  using function = void (*)(Dimension *, std::function<bool(Player &)> &);
+void Dimension::forEachPlayer(std::function<bool(Player &)> callback) {
+  using function = void (*)(Dimension *, std::function<bool(Player &)>);
   auto func = getSign<function>("Dimension::forEachPlayer");
-  func(this, callback);
+  func(this, std::move(callback));
 }
