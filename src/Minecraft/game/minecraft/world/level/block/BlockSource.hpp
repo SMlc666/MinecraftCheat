b76469da17f719fa43a4b7df765494e5c27fbcee
SMlc666 <qq1030452538@outlook.com>
2025-01-25 20:49:59 +0800

    feat(core): 添加BlockSource类及其成员变量

--- src/Minecraft/game/minecraft/world/level/block/BlockSource.hpp
+++ src/Minecraft/game/minecraft/world/level/block/BlockSource.hpp
@@ -1,1 +1,12 @@
 #pragma once
+#include "game/minecraft/actor/actor.hpp"
+#include <cstdint>
+class BlockSource {
+public:
+  uintptr_t **vtable;
+  std::byte padding8[48];
+  Dimension *mDimension;
+  std::byte padding40[248];
+};
+static_assert(offsetof(BlockSource, mDimension) == 0x38);
+static_assert(sizeof(BlockSource) == 0x138);
