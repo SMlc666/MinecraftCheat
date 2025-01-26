2c94a925c7f27bf173de886fc619fd8c847d8ab8
SMlc666 <qq1030452538@outlook.com>
2025-01-26 18:50:46 +0800

    fix: 修正Mob::swing方法中的vtable索引错误

--- src/Minecraft/game/minecraft/actor/mob/mob.cpp
+++ src/Minecraft/game/minecraft/actor/mob/mob.cpp
@@ -1,6 +1,6 @@
 #include "mob.hpp"
 void Mob::swing() {
   using function = void (*)(const Mob *);
-  auto func = reinterpret_cast<function>(this->vtable[90]);
+  auto func = reinterpret_cast<function>(this->vtable[144]);
   return func(this);
 }
