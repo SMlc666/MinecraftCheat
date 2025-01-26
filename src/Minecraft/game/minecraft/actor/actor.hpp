da9828afecad7048cd3c3706177467f9a6390cf5
SMlc666 <qq1030452538@outlook.com>
2025-01-26 20:16:47 +0800

    feat: 添加glm库支持并更新Actor类结构

--- src/Minecraft/game/minecraft/actor/actor.hpp
+++ src/Minecraft/game/minecraft/actor/actor.hpp
@@ -2,6 +2,7 @@
 #include "game/common/gamerefs/WeakRef.hpp"
 #include "game/minecraft/world/level/BlockSourceListener.hpp"
 #include <cstddef>
+#include "glm/glm.hpp"
 #include <memory>
 class Dimension;
 class Level;
@@ -13,8 +14,13 @@ public:
   std::byte padding8[0x2C8];
   Dimension *mDimension;
   WeakRef<Dimension> WeakRef_mDimension;
+  std::byte padding2E8[0x2C];
+  std::byte padding314[0x4];
+  glm::vec3 *mPos;
 
 public:
 }; // namespace class Actor
 static_assert(offsetof(Actor, WeakRef_mDimension) == 0x2D8);
+static_assert(offsetof(Actor, mPos) == 0x318);
+static_assert(sizeof(glm::vec3) == 0xC);
 static_assert(sizeof(WeakRef<Dimension>) == 16);
