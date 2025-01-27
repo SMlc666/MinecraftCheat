faa5227def02d29e267f62073355a0ce9f8a605b
SMlc666 <qq1030452538@outlook.com>
2025-01-27 13:28:35 +0800

    style: 移除不必要的头文件引用

--- src/Minecraft/game/minecraft/actor/actor.cpp
+++ src/Minecraft/game/minecraft/actor/actor.cpp
@@ -1,8 +1,6 @@
 #include "actor.hpp"
-#include "game/minecraft/world/level/dimension/dimension.hpp"
 #include "glm/fwd.hpp"
 #include "gsl/assert"
-#include "signature.hpp"
 glm::vec3 Actor::getPosition() const {
   if (this->mPos == nullptr) {
     gsl::details::terminate();
