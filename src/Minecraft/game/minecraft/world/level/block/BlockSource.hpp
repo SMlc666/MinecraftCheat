7fae75491960fb269c64977eddc76e30ac9f9002
SMlc666 <qq1030452538@outlook.com>
2025-01-27 20:25:36 +0800

    refactor: 移除未使用的成员变量

--- src/Minecraft/game/minecraft/world/level/block/BlockSource.hpp
+++ src/Minecraft/game/minecraft/world/level/block/BlockSource.hpp
@@ -5,10 +5,6 @@ class Block;
 class BlockLegacy;
 class BlockSource {
 public:
-  std::byte padding8[0x18];
-  BlockLegacy *mBlockLegacy;
-
-public:
   virtual ~BlockSource();
   virtual Block *getBlock(int x, int y, int z);
   virtual Block *getBlock(const BlockPos &pos);
