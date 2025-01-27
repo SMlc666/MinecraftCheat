38ff91628d8b8e86298d5206594daf2a946e71b2
SMlc666 <qq1030452538@outlook.com>
2025-01-27 20:16:28 +0800

    refactor: 添加BlockLegacy成员变量及填充字节

--- src/Minecraft/game/minecraft/world/level/block/BlockSource.hpp
+++ src/Minecraft/game/minecraft/world/level/block/BlockSource.hpp
@@ -2,8 +2,12 @@
 #include "game/minecraft/actor/actor.hpp"
 class BlockPos;
 class Block;
+class BlockLegacy;
 class BlockSource {
 public:
+  std::byte padding8[0x18];
+  BlockLegacy *mBlockLegacy;
+
 public:
   virtual ~BlockSource();
   virtual Block *getBlock(int x, int y, int z);
