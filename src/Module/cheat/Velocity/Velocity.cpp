174e96280e72368575f1f53fbeaa132fc3c820c1
SMlc666 <qq1030452538@outlook.com>
2025-01-28 11:16:12 +0800

    feat(cheat): 添加速度模块实现水平和垂直速度修改

--- src/Module/cheat/Velocity/Velocity.cpp
+++ src/Module/cheat/Velocity/Velocity.cpp
@@ -1,1 +1,55 @@
 #include "Velocity.hpp"
+#include "MemTool.hpp"
+#include "Module.hpp"
+#include "game/minecraft/actor/player/localplayer.hpp"
+#include "game/minecraft/client/instance/clientinstance.hpp"
+#include "glm/ext/vector_float3.hpp"
+#include "menu/menu.hpp"
+#include "runtimes/runtimes.hpp"
+#include "signature.hpp"
+#include <any>
+#include <unordered_map>
+class Actor;
+static Module *g_md;
+static const std::unordered_map<std::string, std::any> ConfigData = {
+    {"enabled", false},
+    {"shortcut", false},
+    {"horizontalModify", 0.0F},
+    {"verticalModify", 0.0F},
+};
+MemTool::Hook Actor_lerpMotion_;
+static void Actor_lerpMotion(Actor *actor, glm::vec3 &Motion) {
+  bool enabled = g_md->getGUI().Get<bool>("enabled");
+  if (enabled) {
+    ClientInstance *instance = runtimes::getClientInstance();
+    if (instance != nullptr) {
+      LocalPlayer *player = instance->getLocalPlayer();
+      if (player != nullptr) {
+        if (player == actor) {
+          auto horizontalModify = g_md->getGUI().Get<float>("horizontalModify");
+          auto verticalModify = g_md->getGUI().Get<float>("verticalModify");
+          Motion.x *= horizontalModify;
+          Motion.z *= horizontalModify;
+          Motion.y *= verticalModify;
+        }
+      }
+    }
+  }
+  Actor_lerpMotion_.call<void>(actor, Motion);
+}
+cheat::Velocity::Velocity() : Module("Velocity", MenuType::COMBAT_MENU, ConfigData) {
+  setOnLoad([](Module *module) {
+    g_md = module;
+    void *lerpMotion = getSign<void *>("Actor::lerpMotion");
+    if (lerpMotion != nullptr) {
+      Actor_lerpMotion_ =
+          MemTool::Hook(lerpMotion, reinterpret_cast<void *>(Actor_lerpMotion), nullptr, false);
+    }
+  });
+  setOnEnable([](Module *module) {});
+  setOnDisable([](Module *module) {});
+  setOnDrawGUI([](Module *module) {
+    module->getGUI().SliderFloat("horizontalModify", "水平", 0.0F, 1.0F);
+    module->getGUI().SliderFloat("verticalModify", "垂直", 0.0F, 1.0F);
+  });
+}
--- src/Module/cheat/Velocity/Velocity.hpp
+++ src/Module/cheat/Velocity/Velocity.hpp
@@ -1,1 +1,8 @@
 #pragma once
+#include "Module.hpp"
+namespace cheat {
+class Velocity : public Module {
+public:
+  Velocity();
+};
+} // namespace cheat
