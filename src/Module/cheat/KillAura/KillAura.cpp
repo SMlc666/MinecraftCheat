584ab0b5df3f8a50af744ebea06921a3370b93f3
SMlc666 <qq1030452538@outlook.com>
2025-01-28 11:18:32 +0800

    refactor(killaura): 合并和简化攻击速度设置

--- src/Module/cheat/KillAura/KillAura.cpp
+++ src/Module/cheat/KillAura/KillAura.cpp
@@ -6,7 +6,6 @@
 #include "game/minecraft/actor/player/gamemode/gamemode.hpp"
 #include "game/minecraft/client/instance/clientinstance.hpp"
 #include "game/minecraft/world/level/dimension/dimension.hpp"
-#include "imgui/imgui.h"
 #include "menu/menu.hpp"
 #include "runtimes/runtimes.hpp"
 #include <chrono>
@@ -15,14 +14,8 @@
 #include <random>
 static const std::vector<std::string> PriorityItems = {"Health", "Distance"};
 static const std::unordered_map<std::string, std::any> ConfigData = {
-    {"enabled", false},       {"shortcut", false},
-    {"mincps", 10},           {"maxcps", 20},
-    {"range", 5.0F},          {"swing", false},
-    {"attackNum", 1},         {"antibot", false},
-    {"fov", 180.0F},          {"failurerate", 0},
-    {"priority", 0},          {"rotation", false},
-    {"rotationSpeed", 10.0F}, {"rotationToTarget", false},
-    {"rotationSwing", false}};
+    {"enabled", false}, {"shortcut", false}, {"cps", 10},     {"range", 5.0F},    {"swing", false},
+    {"attackNum", 1},   {"antibot", false},  {"fov", 180.0F}, {"failurerate", 0}, {"priority", 0}};
 static std::vector<Player *> PlayerList = {};
 static std::chrono::steady_clock::time_point LastAttackTime = std::chrono::steady_clock::now();
 static std::random_device g_rd;
@@ -57,23 +50,10 @@ cheat::KillAura::KillAura() : Module("KillAura", MenuType::COMBAT_MENU, ConfigDa
   setOnDrawGUI([](Module *module) {
     auto &gui = module->getGUI();
     gui.SliderFloat("range", "范围", 0.0F, 10.0F);
-    gui.SliderInt("mincps", "最小攻击速度", 1, 20, [module](int value) {
-      int maxcps = module->getGUI().Get<int>("maxcps");
-      if (value > maxcps) {
-        module->getGUI().Set("maxcps", value);
-      }
-    });
-    gui.SliderInt("maxcps", "最大攻击速度", 1, 20);
+    gui.SliderInt("cps", "攻击速度", 1, 20);
     gui.SliderInt("attackNum", "攻击数量", 1, 20);
     gui.CheckBox("swing", "挥手");
     gui.CheckBox("antibot", "反机器人");
-    if (ImGui::TreeNode("Rotation")) {
-      gui.CheckBox("rotation", "转头");
-      gui.CheckBox("rotationToTarget", "只在瞄准到时攻击");
-      gui.CheckBox("rotationSwing", "在转头过程中挥手");
-      gui.SliderFloat("rotationSpeed", "转头速度", 0.0F, 20.0F);
-      ImGui::TreePop();
-    }
     gui.SliderFloat("fov", "视角", 0.0F, 360.0F);
     gui.SliderInt("failurerate", "失败率", 0, 100);
     gui.Selectable("priority", "优先级", PriorityItems);
@@ -82,40 +62,28 @@ cheat::KillAura::KillAura() : Module("KillAura", MenuType::COMBAT_MENU, ConfigDa
     bool enabled = false;
     float Range = NAN;
     bool swing = false;
-    int mincps = 0;
-    int maxcps = 0;
+    int cps = 0;
     int attackNum = 0;
     bool antibot = false;
     int failurerate = 0;
     float fov = NAN;
     int priority = 0;
-    bool rotation = false;
-    float rotationSpeed = NAN;
-    bool rotationToTarget = false;
-    bool rotationSwing = false;
     try {
       enabled = module->getGUI().Get<bool>("enabled");
       Range = module->getGUI().Get<float>("range");
       swing = module->getGUI().Get<bool>("swing");
-      mincps = module->getGUI().Get<int>("mincps");
-      maxcps = module->getGUI().Get<int>("maxcps");
+      cps = module->getGUI().Get<int>("cps");
       attackNum = module->getGUI().Get<int>("attackNum");
       antibot = module->getGUI().Get<bool>("antibot");
       fov = module->getGUI().Get<float>("fov");
       failurerate = module->getGUI().Get<int>("failurerate");
       priority = module->getGUI().Get<int>("priority");
-      rotation = module->getGUI().Get<bool>("rotation");
-      rotationSpeed = module->getGUI().Get<float>("rotationSpeed");
-      rotationToTarget = module->getGUI().Get<bool>("rotationToTarget");
-      rotationSwing = module->getGUI().Get<bool>("rotationSwing");
     } catch (const std::exception &e) {
       return;
     }
     int attackCount = 0;
-    std::uniform_int_distribution<> cps_dist(std::min(mincps, maxcps), maxcps);
-    int random_cps = cps_dist(g_gen);
-    auto interval = std::chrono::milliseconds(1000 / random_cps);
     auto now = std::chrono::steady_clock::now();
+    auto interval = std::chrono::milliseconds(1000 / std::max(cps, 1));
     if (!enabled) {
       return;
     }
