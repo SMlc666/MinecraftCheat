62f35e732c5b3b57815daa2471351678d9db9eeb
SMlc666 <qq1030452538@outlook.com>
2025-01-29 22:48:10 +0800

    refactor: 添加方块放置位置检查逻辑

--- src/Module/cheat/Scaffold/Scaffold.cpp
+++ src/Module/cheat/Scaffold/Scaffold.cpp
@@ -5,19 +5,44 @@
 #include "game/minecraft/world/level/block/Block.hpp"
 #include "game/minecraft/world/level/block/BlockLegacy.hpp"
 #include "game/minecraft/world/level/block/BlockPos.hpp"
+#include "glm/fwd.hpp"
 #include "menu/menu.hpp"
 #include "runtimes/runtimes.hpp"
 #include <unordered_map>
 static Module *g_md{};
 const static std::unordered_map<std::string, std::any> ConfigData = {
     {"enabled", false}, {"shortcut", false}, {"swing", false}};
-static bool canPlace(const glm::vec3 &pos) {
+static const std::unordered_map<glm::ivec3, bool> canPlaceMap = {
+    //以我的世界北方向为准
+    {glm::ivec3(0, -1, 0), true}, //下方
+    {glm::ivec3(0, 1, 0), true},  //上方
+    {glm::ivec3(0, 0, -1), true}, //前方
+    {glm::ivec3(0, 0, 1), true},  //后方
+    {glm::ivec3(-1, 0, 0), true}, //左侧
+    {glm::ivec3(1, 0, 0), true},  //右侧
+};
+static bool isAirBlock(const glm::vec3 &pos) {
   Block *block = runtimes::getClientInstance()->getRegion()->getBlock(
       static_cast<int>(pos.x), static_cast<int>(pos.y), static_cast<int>(pos.z));
   if ((block == nullptr) || (block->mBlockLegacy == nullptr)) {
     return false;
   }
   return block->mBlockLegacy->getName().find("air") != std::string::npos;
+}
+static bool canPlace(const glm::vec3 &pos) {
+  bool can = false;
+  if (!isAirBlock(pos)) {
+    return false;
+  }
+  glm::ivec3 blockPos = pos;
+  for (auto const &pair : canPlaceMap) {
+    blockPos = pos + glm::vec3(pair.first);
+    if (!isAirBlock(glm::vec3(blockPos))) {
+      can = true;
+      break;
+    }
+  }
+  return can;
 }
 static void buildScaffold(LocalPlayer *player, const BlockPos &pos, uchar face) {
   if (auto *gameMode = &player->getGameMode(); gameMode != nullptr) {
