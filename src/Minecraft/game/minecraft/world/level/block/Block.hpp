99c75a08685343791fcae1a496490ca4bf2e11a2
SMlc666 <qq1030452538@outlook.com>
2025-01-29 19:25:16 +0800

    refactor: 添加vtable指针并更新mBlockLegacy类型

--- src/Minecraft/game/minecraft/world/level/block/Block.hpp
+++ src/Minecraft/game/minecraft/world/level/block/Block.hpp
@@ -1,9 +1,11 @@
 #pragma once
+#include "gsl/pointers"
 #include <cstddef>
 class BlockLegacy;
 class Block {
 public:
+  uintptr_t **vtable;
   std::byte padding0[0x20];
-  BlockLegacy *mBlockLegacy;
+  gsl::not_null<BlockLegacy *> mBlockLegacy;
 };
-static_assert(offsetof(Block, mBlockLegacy) == 0x20);
+static_assert(offsetof(Block, mBlockLegacy) == 0x28);
