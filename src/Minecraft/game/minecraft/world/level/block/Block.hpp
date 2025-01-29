56d091a6569757a0d35813b38b8cbcdd65cafec9
SMlc666 <qq1030452538@outlook.com>
2025-01-29 19:30:25 +0800

    style: 添加空行以符合代码格式规范

--- src/Minecraft/game/minecraft/world/level/block/Block.hpp
+++ src/Minecraft/game/minecraft/world/level/block/Block.hpp
@@ -7,5 +7,6 @@ public:
   uintptr_t **vtable;
   std::byte padding0[0x20];
   gsl::not_null<BlockLegacy *> mBlockLegacy; // ida Block::Block a3
+
 };
 static_assert(offsetof(Block, mBlockLegacy) == 0x28);
