0e275064144f340df2f239ef1bb2f52bb6ba1302
SMlc666 <qq1030452538@outlook.com>
2025-01-29 20:05:22 +0800

    refactor(core): 简化getName方法并更新name字段类型

--- src/Minecraft/game/minecraft/world/level/block/BlockLegacy.cpp
+++ src/Minecraft/game/minecraft/world/level/block/BlockLegacy.cpp
@@ -1,6 +1,5 @@
 #include "BlockLegacy.hpp"
 #include <string>
 std::string BlockLegacy::getName() const {
-  const std::string a = *name;
-  return a;
+  return name;
 }
--- src/Minecraft/game/minecraft/world/level/block/BlockLegacy.hpp
+++ src/Minecraft/game/minecraft/world/level/block/BlockLegacy.hpp
@@ -5,9 +5,10 @@ class BlockLegacy {
 public:
   uintptr_t **vtable;
   std::byte padding8[0x20];
-  std::string *name;
+  std::string name;
 
 public:
   [[nodiscard]] std::string getName() const;
 };
 static_assert(offsetof(BlockLegacy, name) == 0x28);
+static_assert(sizeof(std::string) == 24);
