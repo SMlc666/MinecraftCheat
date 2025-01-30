c170b5ef0fc3a78451a1d24bc1db3fc48b394f8e
SMlc666 <qq1030452538@outlook.com>
2025-01-30 22:37:48 +0800

    feat: 添加脚手架模块的空中放置功能

--- src/Module/cheat/Scaffold/Scaffold.cpp
+++ src/Module/cheat/Scaffold/Scaffold.cpp
@@ -1,5 +1,6 @@
 #include "Scaffold.hpp"
 #include "game/minecraft/actor/player/gamemode/gamemode.hpp"
+#include "game/minecraft/actor/provider/ActorCollision.hpp"
 #include "game/minecraft/client/instance/clientinstance.hpp"
 #include "game/minecraft/world/item/ItemStack.hpp"
 #include "Helper/Block/block.hpp"
@@ -10,6 +11,7 @@
 static Module *g_md{};
 const static std::unordered_map<std::string, std::any> ConfigData = {{"enabled", false},
                                                                      {"shortcut", false}};
+static float targetY = 0.5F;
 cheat::Scaffold::Scaffold() : Module("Scaffold", MenuType::COMBAT_MENU, ConfigData) {
   setOnLoad([](Module *module) { g_md = module; });
   setOnEnable([](Module *module) {});
@@ -35,6 +37,14 @@ cheat::Scaffold::Scaffold() : Module("Scaffold", MenuType::COMBAT_MENU, ConfigDa
     if (canBuildBlocks.empty()) {
       return;
     }
+    bool isOnGround = ActorCollision::isOnGround(player->mEntityContext);
+    if (isOnGround || (pos.y - targetY > 0.5F)) {
+      targetY = glm::floor(pos.y) - 0.5F;
+    }
     glm::vec3 motion = player->getMotion();
+    glm::ivec3 targetBlock = {pos.x + motion.x, targetY, pos.z + motion.z};
+    if (Helper::Block::isAirBlock(targetBlock)) {
+      Helper::Block::predictBlock(player, targetBlock, 5.0F);
+    }
   });
 }
