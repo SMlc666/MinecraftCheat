949a22dd5ac065a7cc990a35e4dea87bee54b17d
SMlc666 <qq1030452538@outlook.com>
2025-01-31 17:16:36 +0800

    refactor: 使用MemTool替代直接修改时间缩放

--- src/Module/cheat/Timer/Timer.cpp
+++ src/Module/cheat/Timer/Timer.cpp
@@ -1,4 +1,5 @@
 #include "Timer.hpp"
+#include "MemTool.hpp"
 #include "Module.hpp"
 #include "menu/menu.hpp"
 #include "runtimes/runtimes.hpp"
@@ -6,6 +7,9 @@
 #include "game/minecraft/minecraft.hpp"
 #include "game/minecraft/util/Timer/Timer.hpp"
 #include <unordered_map>
+static const uintptr_t TimerPatch = 0x1D99C60;
+static const float TimerOrigin = 0.34526798129F; //1倍速
+static const float TimerScale = 2.896301001511266F;
 //NOLINTBEGIN
 static std::unordered_map<std::string, std::any> ConfigData = {
     {"enabled", false},
@@ -16,25 +20,28 @@ static std::unordered_map<std::string, std::any> ConfigData = {
 static const float NormalValue = 1.0F;
 cheat::Timer::Timer() : Module("Timer", MenuType::COMBAT_MENU, ConfigData) {
   setOnEnable([](Module *module) {
-    auto *client = runtimes::getClientInstance();
-    if (client != nullptr) {
-      client->minecraftPtr->getRealTimer().setTimeScale(module->getGUI().Get<float>("value"));
-    }
+    auto value = module->getGUI().Get<float>("value");
+    MemTool::write(TimerPatch, value);
   });
-  setOnDisable([](Module *module) {
-    auto *client = runtimes::getClientInstance();
-    if (client != nullptr) {
-      client->minecraftPtr->getRealTimer().setTimeScale(NormalValue);
-    }
-  });
+  setOnDisable([](Module *module) { MemTool::write(TimerPatch, TimerOrigin); });
   setOnDrawGUI([](Module *module) {
     module->getGUI().SliderFloat("value", "value", 0.01, 10, [&module](float value) {
       if (module->getGUI().Get<bool>("enabled")) {
-        auto *client = runtimes::getClientInstance();
-        if (client != nullptr) {
-          client->minecraftPtr->getRealTimer().setTimeScale(value);
-        }
+        MemTool::write(TimerPatch, value / TimerScale);
       }
     });
+  });
+  setOnLoad([](Module *module) {
+    bool enabled;
+    float value;
+    try {
+      enabled = module->getGUI().Get<bool>("enabled");
+      value = module->getGUI().Get<float>("value");
+    } catch (...) {
+      return;
+    }
+    if (enabled) {
+      MemTool::write(TimerPatch, value / TimerScale);
+    }
   });
 }
