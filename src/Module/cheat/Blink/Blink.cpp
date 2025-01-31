989512d13d107be6bb5ea50ac271d073143817e7
SMlc666 <qq1030452538@outlook.com>
2025-01-31 17:55:53 +0800

    refactor: 移除不必要的hook和配置逻辑，优化模块加载

--- src/Module/cheat/Blink/Blink.cpp
+++ src/Module/cheat/Blink/Blink.cpp
@@ -3,26 +3,13 @@
 #include "menu/menu.hpp"
 #include <cstddef>
 #include <sys/socket.h>
-#include <unordered_map>
-#include "MemTool.hpp"
-static bool isBlink;
-MemTool::Hook BlinkSendMsg_;
-static std::unordered_map<std::string, std::any> ConfigData = {
-    {"enabled", false},
-    {"shortcut", false},
-};
-
-static int Blinksendmsg(int fd, const msghdr *msg, int flags) {
-  if (isBlink) {
-    return 0;
-  }
-  return BlinkSendMsg_.call<int>(fd, msg, flags);
-}
 cheat::Blink::Blink() : Module("Blink", MenuType::COMBAT_MENU, ConfigData) {
-  setOnLoad([](Module *module) {
-    BlinkSendMsg_ = MemTool::Hook(MemTool::findSymbol(nullptr, "sendmsg"),
-                                  reinterpret_cast<void *>(Blinksendmsg), nullptr, false);
-  });
+  setOnLoad([](Module *module) {});
   setOnEnable([](Module *module) { isBlink = true; });
   setOnDisable([](Module *module) { isBlink = false; });
+  setOnSendPacket([](Module *module, Packet *packet) {
+    if (packet->getName() == "PlayerAuthInputPacket") {
+      return false;
+    }
+  });
 }
