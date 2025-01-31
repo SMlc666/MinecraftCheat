797bb4e2c06247fa342ddb0d0dc37f78228e4729
SMlc666 <qq1030452538@outlook.com>
2025-01-31 21:23:16 +0800

    refactor: 删除了Block模块的相关代码

--- src/Module/Helper/Block/block.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "base/mcint.hpp"
-#include "game/minecraft/actor/player/player.hpp"
-#include "glm/fwd.hpp"
-namespace Helper::Block {
-bool isAirBlock(glm::ivec3 pos);
-bool buildBlock(glm::ivec3 pos, GameMode *gameMode);
-void predictBlock(glm::ivec3 pos, GameMode *gameMode, float distance);
-} // namespace Helper::Block
