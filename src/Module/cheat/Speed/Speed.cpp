6211f1dc07564e3fbd5f52bc9f6727a27ff512c4
SMlc666 <qq1030452538@outlook.com>
2025-02-01 22:05:56 +0800

    feat(cheat): 添加速度作弊模块

---
+++ src/Module/cheat/Speed/Speed.cpp
@@ -0,0 +1,48 @@
+#include "Speed.hpp"
+#include "game/common/SubClientId.hpp"
+#include "game/minecraft/actor/player/localplayer.hpp"
+#include "game/minecraft/client/instance/clientinstance.hpp"
+#include "menu/menu.hpp"
+#include "Module.hpp"
+#include "runtimes/runtimes.hpp"
+#include <unordered_map>
+static const std::unordered_map<std::string, std::any> ConfigData = {
+    {"enabled", false}, {"shortcut", false}, {"Mode", 0}, {"Speed", 1.0F}};
+static const std::vector<std::string> ModeItems = {
+    "Vanilla",
+};
+static const float NormalSpeed = 1.0F;
+cheat::Speed::Speed() : Module("Speed", MenuType::COMBAT_MENU, ConfigData) {
+  setOnEnable([](Module *module) {
+    float speed{};
+    try {
+      speed = module->getGUI().Get<float>("Speed");
+    } catch (...) {
+      return;
+    }
+    ClientInstance *instance = runtimes::getClientInstance();
+    if (instance == nullptr) {
+      return;
+    }
+    LocalPlayer *player = instance->getLocalPlayer();
+    if (player == nullptr) {
+      return;
+    }
+    player->setSpeed(speed);
+  });
+  setOnDisable([](Module *module) {
+    ClientInstance *instance = runtimes::getClientInstance();
+    if (instance == nullptr) {
+      return;
+    }
+    LocalPlayer *player = instance->getLocalPlayer();
+    if (player == nullptr) {
+      return;
+    }
+    player->setSpeed(NormalSpeed);
+  });
+  setOnDrawGUI([](Module *module) {
+    module->getGUI().Selectable("Mode", "模式", ModeItems);
+    module->getGUI().SliderFloat("Speed", "速度", 0.1F, 10.0F);
+  });
+}
---
+++ src/Module/cheat/Speed/Speed.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "Module.hpp"
+namespace cheat {
+class Speed : public Module {
+public:
+  Speed();
+};
+} // namespace cheat
