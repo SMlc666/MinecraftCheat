2cb6506fcf674af00d3f33148e6f56a8b0b990c5
SMlc666 <qq1030452538@outlook.com>
2025-02-03 18:04:07 +0800

    fix: 修复Swing动画包发送和内存泄漏问题

--- src/Module/cheat/KillAura/KillAura.cpp
+++ src/Module/cheat/KillAura/KillAura.cpp
@@ -145,8 +145,9 @@ cheat::KillAura::KillAura() : Module("KillAura", MenuType::COMBAT_MENU, ConfigDa
         mLocalPlayer->swing();
       }
       if (swing == 1 || swing == 2) {
-        AnimatePacket packet(AnimatePacket::Action::Swing, *mLocalPlayer);
-        mPacketSender->send(packet);
+        auto *packet = new AnimatePacket(AnimatePacket::Action::Swing, *mLocalPlayer);
+        mPacketSender->send(*packet);
+        delete packet;
       }
     }
     if (hasAttacked) {
