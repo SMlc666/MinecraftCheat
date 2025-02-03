e74d78852acf9e5193b53a569ddd3b45865171b2
SMlc666 <qq1030452538@outlook.com>
2025-02-03 21:13:07 +0800

    feat: 添加LoopbackPacketSender的获取与设置函数

--- src/Minecraft/runtimes/runtimes.cpp
+++ src/Minecraft/runtimes/runtimes.cpp
@@ -1,9 +1,17 @@
 #include "runtimes.hpp"
+#include "game/minecraft/network/LoopbackPacketSender.hpp"
 class ClientInstance;
 static ClientInstance *instance = nullptr;
+static LoopbackPacketSender *sender = nullptr;
 ClientInstance *runtimes::getClientInstance() {
   return instance;
 }
 void runtimes::setClientInstance(ClientInstance *minstance) {
   instance = minstance;
+}
+LoopbackPacketSender *runtimes::getPacketSender() {
+  return sender;
+}
+void runtimes::setPacketSender(LoopbackPacketSender *msender) {
+  sender = msender;
 }
--- src/Minecraft/runtimes/runtimes.hpp
+++ src/Minecraft/runtimes/runtimes.hpp
@@ -1,6 +1,9 @@
 #pragma once
+#include "game/minecraft/network/LoopbackPacketSender.hpp"
 class ClientInstance;
 namespace runtimes {
 ClientInstance *getClientInstance();
 void setClientInstance(ClientInstance *minstance);
+LoopbackPacketSender *getPacketSender();
+void setPacketSender(LoopbackPacketSender *msender);
 } // namespace runtimes
