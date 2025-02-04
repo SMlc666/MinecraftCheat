10b2091c80ad11d0964ceaa72324d2c1bddc1fd1
SMlc666 <qq1030452538@outlook.com>
2025-02-04 09:34:34 +0800

    feat: 添加canPlaceBlock功能

--- src/Module/Helper/Block/Block.cpp
+++ src/Module/Helper/Block/Block.cpp
@@ -3,6 +3,7 @@
 #include "game/minecraft/world/level/block/Block.hpp"
 #include "game/minecraft/world/level/block/BlockLegacy.hpp"
 #include "game/minecraft/world/level/block/BlockSource.hpp"
+#include "glm/fwd.hpp"
 #include "runtimes/runtimes.hpp"
 #include <string>
 bool Helper::Block::isAirBlock(glm::ivec3 pos) {
@@ -23,4 +24,24 @@ bool Helper::Block::isAirBlock(glm::ivec3 pos) {
     return false;
   }
   return legacy->getName().find("air") != std::string::npos;
+}
+bool Helper::Block::canPlaceBlock(glm::ivec3 pos) {
+  static const std::vector<glm::ivec3> offsets = {{0, 1, 0},  {0, -1, 0}, {1, 0, 0},
+                                                  {-1, 0, 0}, {0, 0, 1},  {0, 0, -1}};
+  bool canPlace = true;
+  auto *client = runtimes::getClientInstance();
+  if (client == nullptr) {
+    return false;
+  }
+  ::Block *block = client->getRegion()->getBlock(pos);
+  if (block == nullptr) {
+    return false;
+  }
+  if (std::ranges::any_of(offsets, [&](const auto &offset) {
+        auto neighborPos = pos + offset;
+        return !isAirBlock(neighborPos);
+      })) {
+    return true;
+  }
+  return false;
 }
--- src/Module/Helper/Block/Block.hpp
+++ src/Module/Helper/Block/Block.hpp
@@ -2,4 +2,5 @@
 #include "glm/fwd.hpp"
 namespace Helper::Block {
 bool isAirBlock(glm::ivec3 pos);
-}
+bool canPlaceBlock(glm::ivec3 pos);
+} // namespace Helper::Block
