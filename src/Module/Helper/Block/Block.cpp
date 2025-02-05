461038a25f333d8073d8b1d9df1c9696e333c160
SMlc666 <qq1030452538@outlook.com>
2025-02-05 17:37:54 +0800

    feat(core): 添加方块脚手架功能

--- src/Module/Helper/Block/Block.cpp
+++ src/Module/Helper/Block/Block.cpp
@@ -3,6 +3,7 @@
 #include "game/minecraft/world/level/block/Block.hpp"
 #include "game/minecraft/world/level/block/BlockLegacy.hpp"
 #include "game/minecraft/world/level/block/BlockSource.hpp"
+#include "game/minecraft/actor/player/gamemode/gamemode.hpp"
 #include "glm/fwd.hpp"
 #include "runtimes/runtimes.hpp"
 #include <string>
@@ -42,6 +43,28 @@ bool Helper::Block::canPlaceBlock(glm::ivec3 pos) {
         return !isAirBlock(neighborPos);
       })) {
     return true;
+  }
+  return false;
+}
+bool Helper::Block::tryScaffold(LocalPlayer *player, glm::vec3 blockBelow) {
+  blockBelow = glm::floor(blockBelow);
+  BlockPos block(blockBelow);
+  static std::vector<BlockPos> checklist = {
+      BlockPos(0, -1, 0), // 下方
+      BlockPos(0, 1, 0),  // 上方
+      BlockPos(0, 0, -1), // 后方
+      BlockPos(0, 0, 1),  // 前方
+      BlockPos(-1, 0, 0), // 左侧
+      BlockPos(1, 0, 0)   // 右侧
+  };
+  for (const auto &offset : checklist) {
+    BlockPos neighbour = block - offset;
+    if (!isAirBlock(neighbour)) {
+      player->getGameMode().buildBlock(
+          neighbour,
+          std::distance(checklist.begin(), std::find(checklist.begin(), checklist.end(), offset)));
+      return true;
+    }
   }
   return false;
 }
--- src/Module/Helper/Block/Block.hpp
+++ src/Module/Helper/Block/Block.hpp
@@ -1,6 +1,8 @@
 #pragma once
+#include "game/minecraft/actor/player/localplayer.hpp"
 #include "glm/fwd.hpp"
 namespace Helper::Block {
 bool isAirBlock(glm::ivec3 pos);
 bool canPlaceBlock(glm::ivec3 pos);
+bool tryScaffold(LocalPlayer *player, glm::vec3 blockBelow);
 } // namespace Helper::Block
