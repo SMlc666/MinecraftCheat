89038dd0c0bf9bc3df4014041040ed570a30fe60
SMlc666 <qq1030452538@outlook.com>
2025-02-05 16:31:37 +0800

    refactor: 移除不必要的钩子并简化队伍检测逻辑

--- src/Module/cheat/Teams/Teams.cpp
+++ src/Module/cheat/Teams/Teams.cpp
@@ -15,17 +15,13 @@ static const std::unordered_map<std::string, std::any> ConfigData = {
     {"range", 10.0F},
 };
 static std::vector<std::string> TeamNames;
-static MemTool::Hook Target_ProcessPlayer;
-static bool Target_ProcessPlayer_(Player &player, LocalPlayer *localPlayer, bool antibot,
-                                  float range, float fov, std::vector<Player *> &playerList) {
-  std::string name = player.getName();
-  for (const auto &teamName : TeamNames) {
-    if (name == teamName) {
+bool Helper::Target::IsSelfTeam(Player *player) {
+  std::string name = player->getName();
+  for (const auto &name : TeamNames) {
+    if (name == player->getName()) {
       return true;
     }
   }
-  auto ret = Target_ProcessPlayer.call<bool>(player, localPlayer, antibot, range, fov, playerList);
-  return ret;
 }
 cheat::Teams::Teams() : Module("Teams", MenuType::COMBAT_MENU, ConfigData) {
   setOnEnable([](Module *module) {
@@ -66,9 +62,4 @@ cheat::Teams::Teams() : Module("Teams", MenuType::COMBAT_MENU, ConfigData) {
   });
   setOnDisable([](Module *module) {});
   setOnDrawGUI([](Module *module) { module->getGUI().SliderFloat("range", "范围", 1.0F, 30.0F); });
-  setOnLoad([](Module *module) {
-    Target_ProcessPlayer =
-        MemTool::Hook(&Helper::Target::ProcessPlayer,
-                      reinterpret_cast<void *>(&Target_ProcessPlayer_), nullptr, false);
-  });
-}
+};
