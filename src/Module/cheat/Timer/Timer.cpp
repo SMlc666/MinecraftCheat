4681ae91242ed8836969f1c3ef9b54428f9598e0
SMlc666 <qq1030452538@outlook.com>
2025-02-07 21:30:49 +0800

    refactor(cheat): 重构Timer模块以直接操作时间比例

--- src/Module/cheat/Timer/Timer.cpp
+++ src/Module/cheat/Timer/Timer.cpp
@@ -7,9 +7,6 @@
 #include "game/minecraft/minecraft.hpp"
 #include "game/minecraft/util/Timer/Timer.hpp"
 #include <unordered_map>
-static const uintptr_t TimerPatch = 0x1D99C60;
-static const float TimerOrigin = 0.34526798129F; //1倍速
-static const float TimerScale = 2.896301001511266F;
 //NOLINTBEGIN
 static std::unordered_map<std::string, std::any> ConfigData = {
     {"enabled", false},
@@ -17,17 +14,46 @@ static std::unordered_map<std::string, std::any> ConfigData = {
     {"value", 1.0F},
 };
 //NOLINTEND
-static const float NormalValue = 1.0F;
+static const float NormalValue = 20.0F;
 cheat::Timer::Timer() : Module("Timer", MenuType::COMBAT_MENU, ConfigData) {
   setOnEnable([](Module *module) {
-    auto value = module->getGUI().Get<float>("value");
-    MemTool::write(TimerPatch, value);
+    float value = module->getGUI().Get<float>("value");
+    ClientInstance *client = runtimes::getClientInstance();
+    if (!client) {
+      return;
+    }
+    auto *minecraft = client->minecraftPtr;
+    if (!minecraft) {
+      return;
+    }
+    minecraft->mRealTimer.setTimeScale(value);
+    minecraft->mSimTimer.setTimeScale(value);
   });
-  setOnDisable([](Module *module) { MemTool::write(TimerPatch, TimerOrigin); });
+  setOnDisable([](Module *module) {
+    ClientInstance *client = runtimes::getClientInstance();
+    if (!client) {
+      return;
+    }
+    auto *minecraft = client->minecraftPtr;
+    if (!minecraft) {
+      return;
+    }
+    minecraft->mRealTimer.setTimeScale(NormalValue);
+    minecraft->mSimTimer.setTimeScale(NormalValue);
+  });
   setOnDrawGUI([](Module *module) {
-    module->getGUI().SliderFloat("value", "value", 0.01, 10, [&module](float value) {
+    module->getGUI().SliderFloat("value", "value", 0.01F, 80.0F, [&module](float value) {
       if (module->getGUI().Get<bool>("enabled")) {
-        MemTool::write(TimerPatch, value / TimerScale);
+        ClientInstance *client = runtimes::getClientInstance();
+        if (!client) {
+          return;
+        }
+        auto *minecraft = client->minecraftPtr;
+        if (!minecraft) {
+          return;
+        }
+        minecraft->mRealTimer.setTimeScale(value);
+        minecraft->mSimTimer.setTimeScale(value);
       }
     });
   });
@@ -41,7 +67,16 @@ cheat::Timer::Timer() : Module("Timer", MenuType::COMBAT_MENU, ConfigData) {
       return;
     }
     if (enabled) {
-      MemTool::write(TimerPatch, value / TimerScale);
+      ClientInstance *client = runtimes::getClientInstance();
+      if (!client) {
+        return;
+      }
+      auto *minecraft = client->minecraftPtr;
+      if (!minecraft) {
+        return;
+      }
+      minecraft->mRealTimer.setTimeScale(value);
+      minecraft->mSimTimer.setTimeScale(value);
     }
   });
 }
