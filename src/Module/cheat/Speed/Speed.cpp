8a02f62cc051ad9cb24f44634a8e5f9939a6638c
SMlc666 <qq1030452538@outlook.com>
2025-02-09 21:53:01 +0800

    feat(cheat/speed): 添加自定义模式和跳跃高度设置

--- src/Module/cheat/Speed/Speed.cpp
+++ src/Module/cheat/Speed/Speed.cpp
@@ -1,15 +1,18 @@
 #include "Speed.hpp"
 #include "game/common/SubClientId.hpp"
 #include "game/minecraft/actor/player/localplayer.hpp"
+#include "game/minecraft/actor/provider/ActorCollision.hpp"
 #include "game/minecraft/client/instance/clientinstance.hpp"
+#include "imgui/imgui.h"
 #include "menu/menu.hpp"
 #include "Module.hpp"
 #include "runtimes/runtimes.hpp"
 #include <unordered_map>
 static const std::unordered_map<std::string, std::any> ConfigData = {
-    {"enabled", false}, {"shortcut", false}, {"Mode", 0}, {"Speed", 1.0F}};
+    {"enabled", false}, {"shortcut", false}, {"Mode", 0}, {"Speed", 1.0F}, {"HopMotionY", 0.42F}};
 static const std::vector<std::string> ModeItems = {
     "Vanilla",
+    "Custom",
 };
 static const float NormalSpeed = 1.0F;
 cheat::Speed::Speed() : Module("Speed", MenuType::MOVEMENT_MENU, ConfigData) {
@@ -43,7 +46,62 @@ cheat::Speed::Speed() : Module("Speed", MenuType::MOVEMENT_MENU, ConfigData) {
     player->setSpeed(NormalSpeed);
   });
   setOnDrawGUI([](Module *module) {
-    module->getGUI().Selectable("Mode", "模式", ModeItems);
-    module->getGUI().SliderFloat("Speed", "速度", 0.1F, 10.0F);
+    try {
+      module->getGUI().Selectable("Mode", "模式", ModeItems);
+      module->getGUI().SliderFloat("Speed", "速度", 0.1F, 10.0F);
+      if (ImGui::TreeNode("Custom")) {
+        module->getGUI().SliderFloat("HopMotionY", "跳跃高度", 0.1F, 1.0F);
+        ImGui::TreePop();
+      }
+    } catch (...) {
+      return;
+    }
+  });
+  setOnMove([](Module *module, MoveInputHandler *handler) {
+    try {
+      ClientInstance *instance = runtimes::getClientInstance();
+      if (instance == nullptr) {
+        return;
+      }
+      LocalPlayer *player = instance->getLocalPlayer();
+      if (player == nullptr) {
+        return;
+      }
+      int mode = module->getGUI().Get<int>("Mode");
+      float HopMotionY = module->getGUI().Get<float>("HopMotionY");
+      float speed = module->getGUI().Get<float>("Speed");
+      glm::vec3 motion = player->getMotion();
+      float calcYaw = player->getYaw();
+      bool isOnGround = ActorCollision::isOnGround(player->mEntityContext);
+      if (mode == 1) {
+        if (handler->isMove()) {
+          if (isOnGround) {
+            motion.y = HopMotionY;
+          }
+        }
+        if (handler->leftInput())
+          calcYaw -= 90.f;
+        if (handler->rightInput())
+          calcYaw += 90.f;
+        if (handler->forwardLeftInput())
+          calcYaw -= 45.f;
+        if (handler->forwardRightInput())
+          calcYaw += 45.f;
+        calcYaw = glm::radians(calcYaw);
+        motion.x = -sin(calcYaw) * speed;
+        motion.z = cos(calcYaw) * speed;
+        if (handler->backwardInput()) {
+          motion.x = -motion.x;
+          motion.z = -motion.z;
+        }
+      } else {
+        motion.x = 0.0F;
+        motion.z = 0.0F;
+      }
+      player->setMotion(motion);
+      player->lerpMotion(motion);
+    } catch (...) {
+      return;
+    }
   });
 }
