1a2924f939d4641555a1ab89c00a68e7473c1551
SMlc666 <qq1030452538@outlook.com>
2025-02-09 21:16:53 +0800

    fix: 更新ClientInstance结构体成员及偏移量校验

--- src/Minecraft/game/minecraft/client/instance/clientinstance.hpp
+++ src/Minecraft/game/minecraft/client/instance/clientinstance.hpp
@@ -3,15 +3,25 @@
 #include "game/minecraft/world/level/block/BlockSource.hpp"
 #include <cstdint>
 class Minecraft;
+#pragma pack(push, 1)
 class ClientInstance {
 public:
   uintptr_t **vtable;
   std::byte padding0[168];
   Minecraft *minecraftPtr;
+  std::byte paddingB8[0x700];
+  glm::mat4 *mMatrix;
+  std::byte padding7C0[0x38];
+  float *mHfov;
+  std::byte padding800[0xC];
+  float *mVfov;
 
 public:
   [[nodiscard]] LocalPlayer *getLocalPlayer() const;
   [[nodiscard]] BlockSource *getRegion() const;
 };
-
+#pragma pack(pop)
 static_assert(offsetof(ClientInstance, minecraftPtr) == 0xB0, "Minecraft pointer offset is wrong");
+static_assert(offsetof(ClientInstance, mMatrix) == 0x7B8, "Matrix pointer offset is wrong");
+static_assert(offsetof(ClientInstance, mHfov) == 0x7F8, "HFOV pointer offset is wrong");
+static_assert(offsetof(ClientInstance, mVfov) == 0x80C, "VFOV pointer offset is wrong");
