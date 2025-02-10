b0d2c4e8f820b82c016b32b593eebbe5a8e6d32d
SMlc666 <qq1030452538@outlook.com>
2025-02-10 16:51:21 +0800

    fix: 更新ContainerScreenController::tick的签名地址

--- src/Minecraft/signature.cpp
+++ src/Minecraft/signature.cpp
@@ -31,7 +31,7 @@ const std::unordered_map<std::string, uintptr_t> IDApattern = {
     {"Actor::getFallDistance", 0x724D788},
     {"MoveInputHandler::tick", 0x55FBE18},
     {"Player::canOpenContainerScreen", 0x6CF5A3C},
-};
+    {"ContainerScreenController::tick", 0x5C4CB5C}};
 //NOLINTEND(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
 std::unordered_map<std::string, void *> signatures{};
 void signaturesInit() {
