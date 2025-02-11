17f88afcfbe58b5530b2526924880d991c431492
SMlc666 <qq1030452538@outlook.com>
2025-02-11 20:08:38 +0800

    refactor: 移除旧的攻击钩子实现并添加新的攻击回调

--- src/Module/cheat/Seckill/Seckill.cpp
+++ src/Module/cheat/Seckill/Seckill.cpp
@@ -9,43 +9,36 @@
 #include "runtimes/runtimes.hpp"
 #include "signature.hpp"
 #include <unordered_map>
-static MemTool::Hook attack_;
-static Module *g_md;
-bool GameMode_attack(GameMode *self, Actor *entity) {
-  auto ret = attack_.call<bool>(self, entity);
-  bool enabled = g_md->getGUI().Get<bool>("enabled");
-  if (enabled) {
-    bool swing = g_md->getGUI().Get<bool>("swing");
-    int value = g_md->getGUI().Get<int>("value");
-    auto *instance = runtimes::getClientInstance();
-    if (instance != nullptr) {
-      LocalPlayer *player = instance->getLocalPlayer();
-      if (player != nullptr) {
-        if (self->player == player) {
-          for (int i = 0; i < value; i++) {
-            attack_.call<void>(self, entity);
-            if (swing) {
-              player->swing();
-            }
-          }
-        }
-      }
-    }
-  }
-  return ret;
-}
+
 const static std::unordered_map<std::string, std::any> ConfigData = {
     {"enabled", false}, {"swing", false}, {"value", 1}, {"shortcut", false}};
 cheat::Seckill::Seckill() : Module("Seckill", MenuType::COMBAT_MENU, ConfigData) {
-  setOnLoad([](Module *module) {
-    g_md = module;
-    auto *attack_func = getSign<void *>("GameMode::attack");
-    attack_ = MemTool::Hook(attack_func, reinterpret_cast<void *>(GameMode_attack), nullptr, false);
-  });
   setOnEnable([](Module *module) {});
   setOnDisable([](Module *module) {});
   setOnDrawGUI([](Module *module) {
     module->getGUI().SliderInt("value", "连击次数", 0, 100);
     module->getGUI().CheckBox("swing", "挥手");
+  });
+  setOnAttack([](Module *module, MemTool::Hook *mem, Actor *entity) {
+    try {
+      int value = module->getGUI().Get<int>("value");
+      bool swing = module->getGUI().Get<bool>("swing");
+      auto *instance = runtimes::getClientInstance();
+      if (!instance) {
+        return;
+      }
+      LocalPlayer *player = instance->getLocalPlayer();
+      if (!player) {
+        return;
+      }
+      for (int i = 0; i < value; i++) {
+        mem->call<bool>(&player->getGameMode(), entity);
+        if (swing) {
+          player->swing();
+        }
+      }
+    } catch (...) {
+      return;
+    }
   });
 }
--- src/Module/cheat/Seckill/Seckill.hpp
+++ src/Module/cheat/Seckill/Seckill.hpp
@@ -1,7 +1,5 @@
 #pragma once
 #include "Module.hpp"
-#include "game/minecraft/actor/player/gamemode/gamemode.hpp"
-bool GameMode_attack(GameMode *self, Actor *entity);
 namespace cheat {
 class Seckill : public Module {
 public:
