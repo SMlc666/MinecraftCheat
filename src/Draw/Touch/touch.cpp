5de946afbc74a4fac75f6d2e87f02ffa6b76a22b
SMlc666 <qq1030452538@outlook.com>
2025-02-11 16:31:30 +0800

    refactor: 移除不再使用的代码并优化错误处理

--- src/Draw/Touch/touch.cpp
+++ src/Draw/Touch/touch.cpp
@@ -3,13 +3,8 @@
 #include "imgui/backends/imgui_impl_android.h"
 #include "draw.hpp"
 #include "log.hpp"
-#include "iMsgCapture/iMsgCapture.h"
 #include <string>
-#include <vector>
 std::mutex imgui_input_mutex;
-static const int NormalInputUpdateInterval = 1000;
-static const std::vector<std::string> NormalGameActivityName = {
-    "com.mojang.minecraftpe.MainActivity"};
 static void (*old_Input)(void *thiz, void *ex_ab, void *ex_ac);
 static void my_Input(void *thiz, void *ex_ab, void *ex_ac) {
   // 调用原始函数
@@ -20,27 +15,19 @@ static void my_Input(void *thiz, void *ex_ab, void *ex_ac) {
     ImGui_ImplAndroid_HandleInputEvent(static_cast<AInputEvent *>(thiz));
   }
 }
-static void CaptureInput(iMsgEvent *iMsg) {
-  if (is_ImguiSetup) {
-    std::lock_guard<std::mutex> lock(imgui_input_mutex);
-    ImGui_ImplAndroid_HandleInputMsg(iMsg);
-  }
-}
 
 void touchSetup() {
-  void *sym_input = MemTool::findSymbol(
-      "libinput.so",
-      "_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE");
-  if (sym_input != nullptr) {
-    g_log_tool.message(LogLevel::INFO, "drawSetup", std::format("sym_input: {:p}", sym_input));
-    //NOLINTBEGIN
-    MemTool::Hook(sym_input, reinterpret_cast<void *>(my_Input),
-                  reinterpret_cast<void **>(&old_Input), false);
-    //NOLINTEND
-  } else {
-    g_log_tool.message(LogLevel::ERROR, "drawSetup", "sym_input is null");
-    iMsgCapture::instance().setCallback(CaptureInput);
-    iMsgCapture::instance().setUpdateInterval(NormalInputUpdateInterval);
-    iMsgCapture::instance().runThread(NormalGameActivityName, true, 1);
+
+  //NOLINTBEGIN
+  try {
+    MemTool::Hook(
+        "libinput.so",
+        "_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE",
+        reinterpret_cast<void *>(my_Input), reinterpret_cast<void **>(&old_Input), false);
+  } catch (const std::exception &e) {
+    g_log_tool.message(LogLevel::ERROR, "Touch",
+                       "Failed to hook InputConsumer::initializeMotionEvent: " +
+                           std::string(e.what()))
   }
+  //NOLINTEND
 }
