b2758eb6966eafc54d7f14cc755a1ea32d62fa56
SMlc666 <qq1030452538@outlook.com>
2025-02-12 21:04:39 +0800

    feat: 添加NoClip模块功能

---
+++ src/Module/cheat/NoClip/NoClip.cpp
@@ -0,0 +1,36 @@
+#include "NoClip.hpp"
+#include "game/minecraft/actor/player/AbilitiesIndex.hpp"
+#include "game/minecraft/client/instance/clientinstance.hpp"
+#include "menu/menu.hpp"
+#include "runtimes/runtimes.hpp"
+#include <unordered_map>
+namespace {
+const std::unordered_map<std::string, std::any> ConfigData = {
+    {"enabled", false},
+    {"shortcut", false},
+};
+}
+cheat::NoClip::NoClip() : Module("NoClip", MenuType::MOVEMENT_MENU, ConfigData) {
+  setOnEnable([](Module *module) {
+    ClientInstance *instance = runtimes::getClientInstance();
+    if (!instance) {
+      return;
+    }
+    auto *player = instance->getLocalPlayer();
+    if (!player) {
+      return;
+    }
+    player->getAbilities()->setAbility(AbilitiesIndex::NoClip, true);
+  });
+  setOnDisable([](Module *module) {
+    ClientInstance *instance = runtimes::getClientInstance();
+    if (!instance) {
+      return;
+    }
+    auto *player = instance->getLocalPlayer();
+    if (!player) {
+      return;
+    }
+    player->getAbilities()->setAbility(AbilitiesIndex::NoClip, false);
+  });
+}
---
+++ src/Module/cheat/NoClip/NoClip.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "Module.hpp"
+
+namespace cheat {
+class NoClip : public Module {
+public:
+  NoClip();
+};
+} // namespace cheat
