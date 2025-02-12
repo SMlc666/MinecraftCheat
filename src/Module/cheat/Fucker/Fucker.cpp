45124e61d6a1323abbd131832b9e09af49b79cc6
SMlc666 <qq1030452538@outlook.com>
2025-02-12 20:32:08 +0800

    feat: 添加创造模式伪装功能

--- src/Module/cheat/Fucker/Fucker.cpp
+++ src/Module/cheat/Fucker/Fucker.cpp
@@ -3,6 +3,7 @@
 #include "Module.hpp"
 #include "game/minecraft/client/instance/clientinstance.hpp"
 #include "game/minecraft/actor/player/gamemode/gamemode.hpp"
+#include "game/minecraft/world/level/GameType.hpp"
 #include "menu/menu.hpp"
 #include "runtimes/runtimes.hpp"
 #include <any>
@@ -14,15 +15,20 @@ const std::unordered_map<std::string, std::any> ConfigData = {
     {"enabled", false},
     {"shortcut", false},
     {"range", 5.0F},
+    {"SpoofCreative", false},
 };
 }
 cheat::Fucker::Fucker() : Module("Fucker", MenuType::WORLD_MENU, ConfigData) {
-  setOnDrawGUI([](Module *module) { module->getGUI().SliderFloat("range", "范围", 1.0F, 10.0F); });
+  setOnDrawGUI([](Module *module) {
+    module->getGUI().CheckBox("SpoofCreative", "伪装创造模式");
+    module->getGUI().SliderFloat("range", "范围", 1.0F, 10.0F);
+  });
   setOnEnable([](Module *module) {});
   setOnDisable([](Module *module) {});
   setOnTick([](Module *module) {
     try {
       int Range = module->getGUI().Get<float>("range");
+      bool SpoofCreative = module->getGUI().Get<bool>("SpoofCreative");
       ClientInstance *client = runtimes::getClientInstance();
       if (!client)
         return;
@@ -36,6 +42,9 @@ cheat::Fucker::Fucker() : Module("Fucker", MenuType::WORLD_MENU, ConfigData) {
       int endX = pos.x + Range;
       int endY = pos.y + Range;
       int endZ = pos.z + Range;
+      if (SpoofCreative) {
+        player->setPlayerGameType(GameType::Creative);
+      }
       for (int x = startX; x <= endX; ++x) {
         for (int y = startY; y <= endY; ++y) {
           for (int z = startZ; z <= endZ; ++z) {
@@ -46,6 +55,9 @@ cheat::Fucker::Fucker() : Module("Fucker", MenuType::WORLD_MENU, ConfigData) {
             }
           }
         }
+      }
+      if (SpoofCreative) {
+        player->setPlayerGameType(GameType::Default);
       }
     } catch (...) {
       return;
