65ee6228968fb5f23c205801f13e11e2dec55a4d
SMlc666 <qq1030452538@outlook.com>
2025-02-14 13:01:07 +0800

    feat(cheat): 添加伪装创造模式选项

--- src/Module/cheat/Nuker/Nuker.cpp
+++ src/Module/cheat/Nuker/Nuker.cpp
@@ -4,6 +4,7 @@
 #include "game/minecraft/actor/player/localplayer.hpp"
 #include "game/minecraft/actor/player/gamemode/gamemode.hpp"
 #include "game/minecraft/client/instance/clientinstance.hpp"
+#include "game/minecraft/world/level/GameType.hpp"
 #include "runtimes/runtimes.hpp"
 #include <unordered_map>
 namespace {
@@ -11,15 +12,20 @@ std::unordered_map<std::string, std::any> ConfigData = {
     {"enabled", false},
     {"shortcut", false},
     {"range", 10},
+    {"spoofCreative", false},
 };
 }
 cheat::Nuker::Nuker() : Module("Nuker", MenuType::WORLD_MENU, ConfigData) {
-  setOnDrawGUI([](Module *module) { module->getGUI().SliderInt("range", "范围", 1, 20); });
+  setOnDrawGUI([](Module *module) {
+    module->getGUI().CheckBox("spoofCreative", "伪装创造模式");
+    module->getGUI().SliderInt("range", "范围", 1, 20);
+  });
   setOnEnable([](Module *module) {});
   setOnDisable([](Module *module) {});
   setOnTick([](Module *module) {
     try {
       int Range = module->getGUI().Get<int>("range");
+      bool spoofCreative = module->getGUI().Get<bool>("spoofCreative");
       ClientInstance *client = runtimes::getClientInstance();
       if (!client)
         return;
@@ -35,6 +41,8 @@ cheat::Nuker::Nuker() : Module("Nuker", MenuType::WORLD_MENU, ConfigData) {
       int endZ = pos.z + Range;
 
       // 遍历范围内的所有方块
+      if (spoofCreative)
+        player->setPlayerGameType(GameType::Creative);
       for (int x = startX; x <= endX; ++x) {
         for (int y = startY; y <= endY; ++y) {
           for (int z = startZ; z <= endZ; ++z) {
@@ -44,6 +52,7 @@ cheat::Nuker::Nuker() : Module("Nuker", MenuType::WORLD_MENU, ConfigData) {
           }
         }
       }
+      player->setPlayerGameType(GameType::Default);
     } catch (...) {
       return;
     }
